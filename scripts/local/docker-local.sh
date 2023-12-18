d157a195fac9d8f9ff8166af2d4f481290c296ae
Jay <injaesong0@gmail.com>
2023-12-19 03:48:21 +0900

    chore: 로컬 DB와 테스트 DB 컨테이너 생성 스크립트 추가

---
+++ scripts/local/docker-local.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+IMAGE_NAME='ddudu-local'
+CONTAINER_NAMES=("ddudu-local-container" "ddudu-test-container")
+
+for CONTAINER_NAME in "${CONTAINER_NAMES[@]}"; do
+  OLD_CONTAINER="$(docker ps -aq --filter "name=$CONTAINER_NAME")"
+  if [ -n "$OLD_CONTAINER" ]; then
+    docker rm -f "$CONTAINER_NAME" && echo 'Old container has been removed'
+  else
+    echo "There is no existing container with name $CONTAINER_NAME"
+  fi
+done
+
+docker build -t "$IMAGE_NAME" .
+
+LOCAL_PASSWORD='ddudulocal'
+LOCAL_DATABASE='ddudu'
+TEST_PASSWORD='ddudutest'
+TEST_DATABASE='ddudu_test'
+
+# Local DB Container
+docker run --name "${CONTAINER_NAMES[0]}" \
+  -e MYSQL_PASSWORD="$LOCAL_PASSWORD" \
+  -e MYSQL_DATABASE="$LOCAL_DATABASE" \
+  -d -p 13306:3306 "$IMAGE_NAME" \
+  && echo "${CONTAINER_NAMES[0]} created!"
+
+# Test DB Container
+docker run --name "${CONTAINER_NAMES[1]}" \
+  -e MYSQL_PASSWORD="$TEST_PASSWORD" \
+  -e MYSQL_DATABASE="$TEST_DATABASE" \
+  -d -p 13307:3306 "$IMAGE_NAME" \
+  && echo "${CONTAINER_NAMES[1]} created!"
+
