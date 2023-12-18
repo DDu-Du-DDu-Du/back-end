966e62489c5a248c8ebef9fe3f4e5ba67b011b69
Jay <injaesong0@gmail.com>
2023-12-18 14:40:11 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/ddudu/DduduApplication.java
@@ -0,0 +1,13 @@
+package com.ddudu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DduduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DduduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddudu/DduduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddudu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DduduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
