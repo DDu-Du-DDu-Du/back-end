653df182ca291626501cab8f8431d79efb6afd87
uijin-j <uijin0531@gmail.com>
2023-12-19 23:50:27 +0900

    style: DduduStyle에 맞게 코드 포맷 변경

--- src/main/java/com/ddudu/DduduApplication.java
+++ src/main/java/com/ddudu/DduduApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DduduApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DduduApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DduduApplication.class, args);
+  }
 
 }
