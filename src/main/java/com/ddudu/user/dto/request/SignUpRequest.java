4dd74d7198c28dc3d140bd20a426ea8f708f0b86
Jay <injaesong0@gmail.com>
2023-12-22 01:42:16 +0900

    feat: 요청 DTO에 비밀번호 정규식 검증 추가

--- src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
+++ src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
@@ -2,6 +2,7 @@ package com.ddudu.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
@@ -10,6 +11,11 @@ public record SignUpRequest(
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     String email,
     @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
+    @Size(min = 8, max = 50, message = "비밀번호는 8자리 이상이어야 합니다.")
+    @Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@!%*#?&^]).{8,50}$",
+        message = "비밀번호는 영문, 숫자, 특수문자로 구성되어야 합니다."
+    )
     String password,
     @NotBlank(message = "닉네임이 입력되지 않았습니다.")
     @Size(max = 20, message = "닉네임은 최대 20자 입니다.")
