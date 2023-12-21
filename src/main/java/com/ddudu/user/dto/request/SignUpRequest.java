2e340dbc1d1c7ea089cf8b43b296ecaafddce43b
Jay <injaesong0@gmail.com>
2023-12-22 05:38:48 +0900

    chore: 유저 더미 데이터 추가

--- src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
+++ src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
@@ -2,7 +2,6 @@ package com.ddudu.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
