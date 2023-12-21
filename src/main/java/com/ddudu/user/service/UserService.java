06ef302320456260e71b082f0f980de5d2b6870a
Jay <injaesong0@gmail.com>
2023-12-22 00:53:42 +0900

    feat: 회원가입 서비스 구현

---
+++ src/main/java/com/ddudu/user/service/UserService.java
@@ -0,0 +1,44 @@
+package com.ddudu.user.service;
+
+import com.ddudu.user.domain.User;
+import com.ddudu.user.domain.User.UserBuilder;
+import com.ddudu.user.dto.SignUpRequest;
+import com.ddudu.user.repository.UserRepository;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+  public final UserRepository userRepository;
+
+  public User signUp(SignUpRequest request) {
+    if (userRepository.existsByEmail(request.email())) {
+      throw new DuplicateKeyException("이미 존재하는 이메일입니다.");
+    }
+
+    UserBuilder userBuilder = User.builder()
+        .email(request.email())
+        .password(request.password())
+        .nickname(request.nickname())
+        .passwordEncoder(passwordEncoder);
+
+    if (Objects.nonNull(request.optionalUsername())) {
+      if (userRepository.existsByOptionalUsername(request.optionalUsername())) {
+        throw new DuplicateKeyException("이미 존재하는 아이디입니다.");
+      }
+
+      userBuilder.optionalUsername(request.optionalUsername());
+    }
+
+    return userRepository.save(userBuilder.build());
+  }
+
+}
