d296c6073acf939d1a68dfce7c327f0373fb4151
Jay <injaesong0@gmail.com>
2023-12-22 01:21:13 +0900

    feat: 회원가입 api 엔드포인트 추가

---
+++ src/main/java/com/ddudu/user/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.ddudu.user.controller;
+
+import com.ddudu.user.dto.request.SignUpRequest;
+import com.ddudu.user.dto.response.SignUpResponse;
+import com.ddudu.user.service.UserService;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserService userService;
+
+  @PostMapping
+  public ResponseEntity<SignUpResponse> signUp(
+      @RequestBody
+      SignUpRequest request
+  ) {
+    SignUpResponse response = userService.signUp(request);
+    URI uri = URI.create("/api/users/" + response.id());
+    return ResponseEntity.created(uri)
+        .body(response);
+  }
+
+}
