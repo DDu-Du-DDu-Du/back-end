501a8c20f36ce4e8627910aabd153b67f2c91cf5
Jay <injaesong0@gmail.com>
2023-12-23 01:20:13 +0900

    feat: 자기소개가 요청에 존재하면 생성 시 자기소개 추가

--- src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
+++ src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
@@ -20,7 +20,8 @@ public record SignUpRequest(
     String password,
     @NotBlank(message = "닉네임이 입력되지 않았습니다.")
     @Size(max = 20, message = "닉네임은 최대 20자 입니다.")
-    String nickname
+    String nickname,
+    String introduction
 ) {
 
 }
