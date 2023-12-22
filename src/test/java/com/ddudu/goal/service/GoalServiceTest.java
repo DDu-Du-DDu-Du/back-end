2367ecab828d490babc14f7b93e6b909b9797afd
uijin-j <uijin0531@gmail.com>
2023-12-23 03:04:42 +0900

    test: 목표 생성 서비스 테스트 추가
    실패 로직 추가

--- src/test/java/com/ddudu/goal/service/GoalServiceTest.java
+++ src/test/java/com/ddudu/goal/service/GoalServiceTest.java
@@ -3,6 +3,7 @@ package com.ddudu.goal.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.ddudu.goal.domain.Goal;
+import com.ddudu.goal.domain.GoalStatus;
 import com.ddudu.goal.domain.PrivacyType;
 import com.ddudu.goal.dto.requset.CreateGoalRequest;
 import com.ddudu.goal.dto.response.CreateGoalResponse;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,14 +33,18 @@ class GoalServiceTest {
   @Nested
   class 목표_생성_테스트 {
 
+    private String validName;
+    private String validColor;
+
+    목표_생성_테스트() {
+      validName = "dev course";
+      validColor = "F7A29D";
+    }
+
     @Test
     void 목표를_생성할_수_있다() {
       // given
-      String name = "dev course";
-      String color = "191919";
-      PrivacyType privacyType = PrivacyType.PUBLIC;
-
-      CreateGoalRequest request = new CreateGoalRequest(name, color, privacyType);
+      CreateGoalRequest request = new CreateGoalRequest(validName, validColor, PrivacyType.PUBLIC);
 
       // when
       CreateGoalResponse expected = goalService.create(request);
@@ -46,7 +53,77 @@ class GoalServiceTest {
       Optional<Goal> actual = goalRepository.findById(expected.id());
       assertThat(actual).isNotEmpty();
       assertThat(actual.get()).extracting("name", "color")
-          .containsExactly(name, color);
+          .containsExactly(validName, validColor);
+    }
+
+    @Test
+    void 목표_생성_시_ID가_자동_생성된다() {
+      // given
+      CreateGoalRequest request = new CreateGoalRequest(validName, validColor, PrivacyType.PUBLIC);
+
+      // when
+      CreateGoalResponse expected = goalService.create(request);
+
+      // then
+      Optional<Goal> actual = goalRepository.findById(expected.id());
+      assertThat(actual).isNotEmpty();
+      assertThat(actual.get()
+          .getId()).isNotNull();
+    }
+
+    @Test
+    void 목표_생성_시_IN_PROGRESS_상태가_된다() {
+      // given
+      CreateGoalRequest request = new CreateGoalRequest(validName, validColor, PrivacyType.PUBLIC);
+
+      // when
+      CreateGoalResponse expected = goalService.create(request);
+
+      // then
+      Optional<Goal> actual = goalRepository.findById(expected.id());
+      assertThat(actual).isNotEmpty();
+      assertThat(actual.get()
+          .getStatus()).isEqualTo(GoalStatus.IN_PROGRESS);
+    }
+
+    @ParameterizedTest(name = "유효하지 않은 색상 : {0}")
+    @NullAndEmptySource
+    void 색상을_설정하지_않거나_빈_문자열이면_기본값이_적용된다(String invalidColor) {
+      // given
+      String defaultColor = "191919";
+
+      CreateGoalRequest request = CreateGoalRequest.builder()
+          .name(validName)
+          .color(invalidColor)
+          .privacyType(PrivacyType.PUBLIC)
+          .build();
+
+      // when
+      CreateGoalResponse expected = goalService.create(request);
+
+      // then
+      Optional<Goal> actual = goalRepository.findById(expected.id());
+      assertThat(actual).isNotEmpty();
+      assertThat(actual.get()
+          .getColor()).isEqualTo(defaultColor);
+    }
+
+    @Test
+    void 보기_설정을_설정하지_않으면_PRIVATE이_적용된다() {
+      // given
+      CreateGoalRequest request = CreateGoalRequest.builder()
+          .name(validName)
+          .color(validColor)
+          .build();
+
+      // when
+      CreateGoalResponse expected = goalService.create(request);
+
+      // then
+      Optional<Goal> actual = goalRepository.findById(expected.id());
+      assertThat(actual).isNotEmpty();
+      assertThat(actual.get()
+          .getPrivacyType()).isEqualTo(PrivacyType.PRIVATE);
     }
 
   }
