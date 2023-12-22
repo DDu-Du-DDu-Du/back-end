0ab73c19ff59407f19864575d4bcde529f5ae9b6
uijin-j <uijin0531@gmail.com>
2023-12-23 02:51:20 +0900

    test: 목표 생성 컨트롤러 테스트 추가
    실패로직 추가

--- src/test/java/com/ddudu/goal/controller/GoalControllerTest.java
+++ src/test/java/com/ddudu/goal/controller/GoalControllerTest.java
@@ -1,20 +1,27 @@
 package com.ddudu.goal.controller;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.ddudu.config.WebSecurityConfig;
+import com.ddudu.goal.domain.PrivacyType;
 import com.ddudu.goal.dto.requset.CreateGoalRequest;
 import com.ddudu.goal.dto.response.CreateGoalResponse;
 import com.ddudu.goal.service.GoalService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -39,16 +46,24 @@ class GoalControllerTest {
   @Nested
   class 목표_생성_API_테스트 {
 
+    private String validName;
+    private String validColor;
+
+    목표_생성_API_테스트() {
+      validName = "dev course";
+      validColor = "F7A29D";
+    }
+
     @Test
-    void 목표를_생성할_수_있다() throws Exception {
+    void 목표_색상_보기_설정과_함께_목표를_생성할_수_있다() throws Exception {
       // given
       CreateGoalRequest request = CreateGoalRequest.builder()
-          .name("dev course")
-          .color(null)
-          .privacyType(null)
+          .name(validName)
+          .color(validColor)
+          .privacyType(PrivacyType.PUBLIC)
           .build();
 
-      CreateGoalResponse response = new CreateGoalResponse(1L, "dev course", "191919");
+      CreateGoalResponse response = new CreateGoalResponse(1L, validName, validColor);
 
       given(goalService.create(any(CreateGoalRequest.class)))
           .willReturn(response);
@@ -60,8 +75,89 @@ class GoalControllerTest {
                   .contentType(MediaType.APPLICATION_JSON)
           )
           .andExpect(status().isCreated())
-          .andExpect(jsonPath("$.id").value("1"));
+          .andExpect(header().exists("location"))
+          .andExpect(jsonPath("$.id").value(response.id()))
+          .andExpect(jsonPath("$.name").value(response.name()))
+          .andExpect(jsonPath("$.color").value(response.color()));
     }
+
+    @ParameterizedTest(name = "유효하지 않은 목표 : {0}")
+    @NullAndEmptySource
+    void 목표가_입력되지_않거나_빈_문자열일_수_없다(String invalidName) throws Exception {
+      // given
+      CreateGoalRequest request = CreateGoalRequest.builder()
+          .name(invalidName)
+          .color(validColor)
+          .privacyType(PrivacyType.PUBLIC)
+          .build();
+
+      given(goalService.create(any(CreateGoalRequest.class)))
+          .willReturn(new CreateGoalResponse(1L, validName, validColor));
+
+      // when then
+      mockMvc.perform(
+              post("/api/goals")
+                  .content(objectMapper.writeValueAsString(request))
+                  .contentType(MediaType.APPLICATION_JSON)
+          )
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.message")
+              .value(containsString("목표가 입력되지 않았습니다.")));
+    }
+
+    @ParameterizedTest(name = "50자를 초과하는 목표 : {0}")
+    @MethodSource("provideLongString")
+    void 목표는_50자를_넘을_수_없다(String longName) throws Exception {
+      // given
+      CreateGoalRequest request = CreateGoalRequest.builder()
+          .name(longName)
+          .color(validColor)
+          .privacyType(PrivacyType.PUBLIC)
+          .build();
+
+      given(goalService.create(any(CreateGoalRequest.class)))
+          .willReturn(new CreateGoalResponse(1L, validName, validColor));
+
+      // when then
+      mockMvc.perform(
+              post("/api/goals")
+                  .content(objectMapper.writeValueAsString(request))
+                  .contentType(MediaType.APPLICATION_JSON)
+          )
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.message")
+              .value(containsString("목표는 최대 50자 입니다.")));
+    }
+
+    @ParameterizedTest(name = "6자를 초과하는 색상 : {0}")
+    @MethodSource("provideLongString")
+    void 색상은_6자를_넘을_수_없다(String longColor) throws Exception {
+      // given
+      CreateGoalRequest request = CreateGoalRequest.builder()
+          .color(longColor)
+          .name(validName)
+          .privacyType(PrivacyType.PUBLIC)
+          .build();
+
+      given(goalService.create(any(CreateGoalRequest.class)))
+          .willReturn(new CreateGoalResponse(1L, validName, validColor));
+
+      // when then
+      mockMvc.perform(
+              post("/api/goals")
+                  .content(objectMapper.writeValueAsString(request))
+                  .contentType(MediaType.APPLICATION_JSON)
+          )
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.message")
+              .value(containsString("색상 코드는 6자리 16진수입니다.")));
+    }
+
+    private static List<String> provideLongString() {
+      String longString = "a".repeat(100);
+      return List.of(longString);
+    }
+
 
   }
 
