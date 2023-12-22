0287c8317be152993b2533298ff3ef9a8857e8c5
Jay <injaesong0@gmail.com>
2023-12-23 01:21:02 +0900

    feat: 회원가입 요청에 자기소개 검증 추가

--- src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
+++ src/main/java/com/ddudu/user/dto/request/SignUpRequest.java
@@ -21,6 +21,7 @@ public record SignUpRequest(
     @NotBlank(message = "닉네임이 입력되지 않았습니다.")
     @Size(max = 20, message = "닉네임은 최대 20자 입니다.")
     String nickname,
+    @Size(max = 50, message = "자기소개는 최대 50자 입니다.")
     String introduction
 ) {
 
