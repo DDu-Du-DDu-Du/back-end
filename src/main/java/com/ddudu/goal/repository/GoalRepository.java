ab5d060855e0c3a37a7b7bcb1d015b240a353c1f
uijin-j <uijin0531@gmail.com>
2023-12-23 20:06:19 +0900

    refactor: 불필요한 애너테이션 제거
    Repository에 @Repository 애너테이션 제거

--- src/main/java/com/ddudu/goal/repository/GoalRepository.java
+++ src/main/java/com/ddudu/goal/repository/GoalRepository.java
@@ -2,9 +2,7 @@ package com.ddudu.goal.repository;
 
 import com.ddudu.goal.domain.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
 
 }
