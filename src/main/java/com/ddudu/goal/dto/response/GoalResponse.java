32a5c5a6833aa15f7ca56774fd5e10b3330c7ba0
uijin-j <uijin0531@gmail.com>
2023-12-26 02:48:29 +0900

    feat: 단일 목표 조회 응답 DTO 구현

---
+++ src/main/java/com/ddudu/goal/dto/response/GoalResponse.java
@@ -0,0 +1,27 @@
+package com.ddudu.goal.dto.response;
+
+import com.ddudu.goal.domain.Goal;
+import lombok.Builder;
+
+@Builder
+public record GoalResponse(
+    Long id,
+    String name,
+    String status,
+    String color,
+    String privacyType
+) {
+
+  public static GoalResponse from(Goal goal) {
+    return GoalResponse.builder()
+        .id(goal.getId())
+        .name(goal.getName())
+        .status(goal.getStatus()
+            .name())
+        .color(goal.getColor())
+        .privacyType(goal.getPrivacyType()
+            .name())
+        .build();
+  }
+
+}
