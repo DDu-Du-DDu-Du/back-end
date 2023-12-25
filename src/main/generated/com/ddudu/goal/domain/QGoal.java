144ea8db7b53894914f3bf1796ae176aec1839ae
EomYerim <djadpfa1002@naver.com>
2023-12-26 00:01:12 +0900

    chore: querydsl 의존성 추가 및 QClass (자동) 추가

---
+++ src/main/generated/com/ddudu/goal/domain/QGoal.java
@@ -0,0 +1,59 @@
+package com.ddudu.goal.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QGoal is a Querydsl query type for Goal
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QGoal extends EntityPathBase<Goal> {
+
+    private static final long serialVersionUID = -71041929L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QGoal goal = new QGoal("goal");
+
+    public final QColor color;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isDeleted = createBoolean("isDeleted");
+
+    public final StringPath name = createString("name");
+
+    public final EnumPath<PrivacyType> privacyType = createEnum("privacyType", PrivacyType.class);
+
+    public final EnumPath<GoalStatus> status = createEnum("status", GoalStatus.class);
+
+    public QGoal(String variable) {
+        this(Goal.class, forVariable(variable), INITS);
+    }
+
+    public QGoal(Path<? extends Goal> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QGoal(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QGoal(PathMetadata metadata, PathInits inits) {
+        this(Goal.class, metadata, inits);
+    }
+
+    public QGoal(Class<? extends Goal> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.color = inits.isInitialized("color") ? new QColor(forProperty("color")) : null;
+    }
+
+}
+
