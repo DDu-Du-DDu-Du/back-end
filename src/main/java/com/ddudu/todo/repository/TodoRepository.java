e2e9349c2c4f6717f471c6e4f7a9ac9e78f65d52
EomYerim <djadpfa1002@naver.com>
2023-12-26 01:50:12 +0900

    feat: Todo repository QueryDSL을 사용해 일별 할 일 리스트 조회 기능 추가

--- src/main/java/com/ddudu/todo/repository/TodoRepository.java
+++ src/main/java/com/ddudu/todo/repository/TodoRepository.java
@@ -3,6 +3,6 @@ package com.ddudu.todo.repository;
 import com.ddudu.todo.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
 }
