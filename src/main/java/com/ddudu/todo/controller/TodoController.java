b8e4858d7790d13347e40be5f1515cfda0e9f12f
EomYerim <djadpfa1002@naver.com>
2023-12-26 01:51:49 +0900

    feat: Todo 일별 할 일 리스트 조회 기능 추가

--- src/main/java/com/ddudu/todo/controller/TodoController.java
+++ src/main/java/com/ddudu/todo/controller/TodoController.java
@@ -1,14 +1,19 @@
 package com.ddudu.todo.controller;
 
+import com.ddudu.todo.dto.response.TodoListResponse;
 import com.ddudu.todo.dto.response.TodoResponse;
 import com.ddudu.todo.service.TodoService;
 import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,13 +24,45 @@ public class TodoController {
   private final TodoService todoService;
 
   @GetMapping("/{id}")
-  public ResponseEntity<?> getTodo(@PathVariable Long id) {
+  public ResponseEntity<?> getTodo(
+      @PathVariable
+          Long id
+  ) {
     try {
       TodoResponse response = todoService.findById(id);
       return ResponseEntity.ok(response);
     } catch (EntityNotFoundException e) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND)
           .body(e.getMessage());
+    }
+  }
+
+  @GetMapping
+  public ResponseEntity<?> getDailyTodoList(
+      @RequestParam(name = "date", required = false)
+          String date
+  ) {
+    try {
+      validateDate(date);
+
+      List<TodoListResponse> response = todoService.findDailyTodoList(date);
+      return ResponseEntity.ok(response);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body(e.getMessage());
+    } catch (DateTimeParseException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("유효하지 않은 날짜입니다.");
+    }
+  }
+
+  private void validateDate(String date) {
+    if (date != null && !date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+      throw new IllegalArgumentException("날짜 형식이 올바르지 않습니다. YYYY-MM-DD 형식으로 입력해주세요.");
+    }
+
+    if (date != null) {
+      LocalDate.parse(date);
     }
   }
 
--- src/main/java/com/ddudu/todo/service/TodoService.java
+++ src/main/java/com/ddudu/todo/service/TodoService.java
@@ -1,9 +1,19 @@
 package com.ddudu.todo.service;
 
+import com.ddudu.goal.domain.Goal;
+import com.ddudu.goal.repository.GoalRepository;
 import com.ddudu.todo.domain.Todo;
+import com.ddudu.todo.dto.response.TodoInfo;
+import com.ddudu.todo.dto.response.TodoListResponse;
 import com.ddudu.todo.dto.response.TodoResponse;
 import com.ddudu.todo.repository.TodoRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +24,41 @@ import org.springframework.transaction.annotation.Transactional;
 public class TodoService {
 
   private final TodoRepository todoRepository;
+  private final GoalRepository goalRepository;
 
   public TodoResponse findById(Long id) {
     Todo todo = todoRepository.findById(id)
         .orElseThrow(() -> new EntityNotFoundException("할 일 아이디가 존재하지 않습니다."));
 
     return TodoResponse.from(todo);
+  }
+
+  public List<TodoListResponse> findDailyTodoList(String dateString) {
+    LocalDate date = parseDate(dateString);
+    List<Goal> goals = goalRepository.findAll();
+    List<Todo> todos = todoRepository.findTodosByDate(
+        date.atStartOfDay(), date.atTime(LocalTime.MAX)
+    );
+
+    Map<Long, List<Todo>> todosByGoal = todos.stream()
+        .collect(Collectors.groupingBy(todo -> todo.getGoal()
+            .getId()));
+
+    return goals.stream()
+        .map(goal -> {
+          List<TodoInfo> todoInfos = todosByGoal.getOrDefault(goal.getId(), List.of())
+              .stream()
+              .map(TodoInfo::from)
+              .collect(Collectors.toList());
+
+          return TodoListResponse.from(goal, todoInfos);
+        })
+        .collect(Collectors.toList());
+  }
+
+  private LocalDate parseDate(String dateString) {
+    return (dateString == null || dateString.isEmpty()) ? LocalDate.now()
+        : LocalDate.parse(dateString, DateTimeFormatter.ISO_DATE);
   }
 
 }
