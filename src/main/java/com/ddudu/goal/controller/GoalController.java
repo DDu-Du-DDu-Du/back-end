2f1dd2c59d45587cd08deeebed4539b488f6ad7d
uijin-j <uijin0531@gmail.com>
2023-12-26 02:51:11 +0900

    feat: 단일 목표 조회 API 엔드포인트 추가

--- src/main/java/com/ddudu/goal/controller/GoalController.java
+++ src/main/java/com/ddudu/goal/controller/GoalController.java
@@ -1,9 +1,13 @@
 package com.ddudu.goal.controller;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 import com.ddudu.goal.dto.requset.CreateGoalRequest;
 import com.ddudu.goal.dto.response.CreateGoalResponse;
 import com.ddudu.goal.dto.response.ErrorResponse;
+import com.ddudu.goal.dto.response.GoalResponse;
 import com.ddudu.goal.service.GoalService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +16,8 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +44,16 @@ public class GoalController {
         .body(response);
   }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<GoalResponse> getGoal(
+      @PathVariable
+      Long id
+  ) {
+    GoalResponse response = goalService.getGoal(id);
+
+    return ResponseEntity.ok(response);
+  }
+
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<ErrorResponse> handleIllegalArgumentEx(IllegalArgumentException e) {
     ErrorResponse response = new ErrorResponse(e.getMessage());
@@ -60,6 +76,14 @@ public class GoalController {
     ErrorResponse response = new ErrorResponse(errorMessage);
 
     return ResponseEntity.badRequest()
+        .body(response);
+  }
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleEntityNotFoundEx(EntityNotFoundException e) {
+    ErrorResponse response = new ErrorResponse(e.getMessage());
+
+    return ResponseEntity.status(NOT_FOUND)
         .body(response);
   }
 
