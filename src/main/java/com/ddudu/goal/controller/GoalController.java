2d593a931edf31529b7f0cb70efefba5e7708660
uijin-j <uijin0531@gmail.com>
2023-12-27 06:28:22 +0900

    feat: 목표 수정 API HTTP 메서드 변경 Patch -> Put

--- src/main/java/com/ddudu/goal/controller/GoalController.java
+++ src/main/java/com/ddudu/goal/controller/GoalController.java
@@ -14,9 +14,9 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +42,7 @@ public class GoalController {
         .body(response);
   }
 
-  @PatchMapping("/{id}")
+  @PutMapping("/{id}")
   public ResponseEntity<GoalResponse> update(
       @PathVariable
       Long id,
