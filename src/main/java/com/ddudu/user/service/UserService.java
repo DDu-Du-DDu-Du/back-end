18eaebd963df961e3177882ab29d3966fb1e2081
Jay <injaesong0@gmail.com>
2023-12-28 02:32:25 +0900

    feat: 토큰으로 유저 불러오기 구현

--- src/main/java/com/ddudu/user/service/UserService.java
+++ src/main/java/com/ddudu/user/service/UserService.java
@@ -1,25 +1,30 @@
 package com.ddudu.user.service;
 
+import com.ddudu.auth.jwt.JwtAuthToken;
 import com.ddudu.user.domain.Email;
 import com.ddudu.user.domain.User;
 import com.ddudu.user.domain.User.UserBuilder;
 import com.ddudu.user.dto.request.SignUpRequest;
 import com.ddudu.user.dto.response.SignUpResponse;
+import com.ddudu.user.dto.response.UserResponse;
 import com.ddudu.user.repository.UserRepository;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
+  @Transactional
   public SignUpResponse signUp(SignUpRequest request) {
     Email email = new Email(request.email());
 
@@ -46,6 +51,14 @@ public class UserService {
     }
 
     return SignUpResponse.from(userRepository.save(userBuilder.build()));
+  }
+
+  public UserResponse loadFromToken(JwtAuthToken jwtAuthToken) {
+    Long userId = jwtAuthToken.getUserId();
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new AccessDeniedException("잘못된 토큰입니다."));
+
+    return UserResponse.from(user);
   }
 
 }
