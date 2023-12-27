69d952143d1ddb81241ae6e5083f81066da8322d
EomYerim <djadpfa1002@naver.com>
2023-12-27 10:59:52 +0900

    feat: GoalInfo에 color 추가

--- src/main/java/com/ddudu/todo/dto/response/GoalInfo.java
+++ src/main/java/com/ddudu/todo/dto/response/GoalInfo.java
@@ -4,12 +4,17 @@ import com.ddudu.goal.domain.Goal;
 import lombok.Builder;
 
 @Builder
-public record GoalInfo(Long id, String name) {
+public record GoalInfo(
+    Long id,
+    String name,
+    String color
+) {
 
   public static GoalInfo from(Goal goal) {
     return GoalInfo.builder()
         .id(goal.getId())
         .name(goal.getName())
+        .color(goal.getColor())
         .build();
   }
 
