c10bdade687f4e315bfd3d6b0e35d16ff904b86a
EomYerim <djadpfa1002@naver.com>
2023-12-27 11:12:58 +0900

    refactor: Todo controller 불필요한 어노테이션 제거

--- src/main/java/com/ddudu/todo/controller/TodoController.java
+++ src/main/java/com/ddudu/todo/controller/TodoController.java
@@ -7,14 +7,12 @@ import jakarta.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
@@ -41,9 +39,7 @@ public class TodoController {
 
   @GetMapping
   public ResponseEntity<?> getDailyTodoList(
-      @RequestParam(required = false)
-      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-          LocalDate date
+      LocalDate date
   ) {
     date = (date == null) ? LocalDate.now() : date;
 
