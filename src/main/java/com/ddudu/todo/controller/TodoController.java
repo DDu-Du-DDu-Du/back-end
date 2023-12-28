9a9f084f45c6c1dc81164f276bb4e79cbe6ca5fe
EomYerim <djadpfa1002@naver.com>
2023-12-29 03:17:47 +0900

    feat: Todo controller 주간 및 월간 달성률 조회 기능 추가

--- src/main/java/com/ddudu/todo/controller/TodoController.java
+++ src/main/java/com/ddudu/todo/controller/TodoController.java
@@ -1,12 +1,16 @@
 package com.ddudu.todo.controller;
 
+import com.ddudu.todo.dto.response.TodoCompletionResponse;
 import com.ddudu.todo.dto.response.TodoListResponse;
 import com.ddudu.todo.dto.response.TodoResponse;
 import com.ddudu.todo.service.TodoService;
 import jakarta.persistence.EntityNotFoundException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
@@ -50,6 +55,29 @@ public class TodoController {
   ) {
     TodoResponse response = todoService.updateStatus(id);
     return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/weekly")
+  public ResponseEntity<List<TodoCompletionResponse>> getWeeklyTodoCompletion(
+      LocalDate date
+  ) {
+    date = (date == null) ? LocalDate.now()
+        .with(DayOfWeek.MONDAY) : date.with(DayOfWeek.MONDAY);
+
+    List<TodoCompletionResponse> completionList = todoService.findWeeklyTodoCompletion(date);
+    return ResponseEntity.ok(completionList);
+  }
+
+  @GetMapping("/monthly")
+  public ResponseEntity<List<TodoCompletionResponse>> getMonthlyTodoCompletion(
+      @RequestParam(value = "date", required = false)
+      @DateTimeFormat(pattern = "yyyy-MM")
+          YearMonth yearMonth
+  ) {
+    yearMonth = (yearMonth == null) ? YearMonth.now() : yearMonth;
+
+    List<TodoCompletionResponse> completionList = todoService.findMonthlyTodoCompletion(yearMonth);
+    return ResponseEntity.ok(completionList);
   }
 
   @ExceptionHandler(EntityNotFoundException.class)
