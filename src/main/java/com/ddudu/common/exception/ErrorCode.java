530649de74c3829aa92807b62208502b34b10362
Jay <injaesong0@gmail.com>
2023-12-29 02:47:45 +0900

    feat: 도메인 별 에러 코드 구현

--- src/main/java/com/ddudu/common/exception/ErrorCode.java
+++ src/main/java/com/ddudu/common/exception/ErrorCode.java
@@ -5,4 +5,5 @@ public interface ErrorCode {
   int getCode();
 
   String getMessage();
+
 }
--- src/main/java/com/ddudu/goal/exception/GoalErrorCode.java
+++ src/main/java/com/ddudu/goal/exception/GoalErrorCode.java
@@ -7,7 +7,10 @@ public enum GoalErrorCode implements ErrorCode {
   BLANK_NAME(3001, "목표명은 필수값입니다."),
   EXCESSIVE_NAME_LENGTH(3002, "목표명이 최대 글자수를 초과했습니다."),
   INVALID_COLOR_FORMAT(3003, "올바르지 않은 색상 코드입니다."),
-  ID_NOT_EXISTING(3004, "해당 아이디를 가진 목표가 존재하지 않습니다.");
+  ID_NOT_EXISTING(3004, "해당 아이디를 가진 목표가 존재하지 않습니다."),
+  NULL_STATUS(3005, "목표 상태가 입력되지 않았습니다."),
+  BLANK_COLOR(3006, "색상이 입력되지 않았습니다."),
+  NULL_PRIVACY_TYPE(3007, "공개 설정이 입력되지 않았습니다.");
 
   private final int code;
   private final String message;
@@ -26,4 +29,5 @@ public enum GoalErrorCode implements ErrorCode {
   public String getMessage() {
     return message;
   }
+
 }
