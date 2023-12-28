196a64b0572d72751c22fae2cec7d0d4208894fa
EomYerim <djadpfa1002@naver.com>
2023-12-29 03:17:01 +0900

    feat: Todo repository QueryDSL 사용해 기간별 달성률 조회 추가

--- src/main/java/com/ddudu/todo/repository/TodoRepositoryCustom.java
+++ src/main/java/com/ddudu/todo/repository/TodoRepositoryCustom.java
@@ -1,11 +1,14 @@
 package com.ddudu.todo.repository;
 
 import com.ddudu.todo.domain.Todo;
+import com.ddudu.todo.dto.response.TodoCompletionResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TodoRepositoryCustom {
-  
+
   List<Todo> findTodosByDate(LocalDateTime startDate, LocalDateTime endDate);
+
+  List<TodoCompletionResponse> findTodosCompletion(LocalDateTime startDate, LocalDateTime endDate);
 
 }
--- src/main/java/com/ddudu/todo/repository/TodoRepositoryImpl.java
+++ src/main/java/com/ddudu/todo/repository/TodoRepositoryImpl.java
@@ -2,8 +2,13 @@ package com.ddudu.todo.repository;
 
 import com.ddudu.todo.domain.QTodo;
 import com.ddudu.todo.domain.Todo;
+import com.ddudu.todo.domain.TodoStatus;
+import com.ddudu.todo.dto.response.TodoCompletionResponse;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberTemplate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.stereotype.Repository;
@@ -32,5 +37,54 @@ public class TodoRepositoryImpl implements TodoRepositoryCustom {
         .orderBy(todo.status.desc(), todo.endAt.asc())
         .fetch();
   }
+
+  @Override
+  public List<TodoCompletionResponse> findTodosCompletion(
+      LocalDateTime startDate, LocalDateTime endDate
+  ) {
+    QTodo todo = QTodo.todo;
+
+    NumberTemplate<Long> totalTodosTemplate = Expressions.numberTemplate(
+        Long.class,
+        "COUNT({0})", todo.id
+    );
+
+    NumberTemplate<Long> uncompletedTodosTemplate = Expressions.numberTemplate(
+        Long.class,
+        "COUNT(DISTINCT CASE WHEN {0} = {1} THEN {2} END)",
+        todo.status,
+        TodoStatus.UNCOMPLETED,
+        todo.id
+    );
+
+    return jpaQueryFactory
+        .select(
+            Expressions.dateTemplate(LocalDate.class, "{0}", todo.beginAt)
+                .as("date"),
+            totalTodosTemplate
+                .as("totalTodos"),
+            uncompletedTodosTemplate
+                .as("uncompletedTodos")
+        )
+        .from(todo)
+        .where(
+            todo.beginAt.between(startDate, endDate),
+            todo.isDeleted.eq(false)
+        )
+        .groupBy(Expressions.dateTemplate(LocalDate.class, "{0}", todo.beginAt))
+        .fetch()
+        .stream()
+        .map(result -> TodoCompletionResponse.builder()
+            .date(result.get(0, LocalDateTime.class)
+                .toLocalDate()
+                .toString())
+            .totalTodos(result.get(1, Long.class)
+                .intValue())
+            .uncompletedTodos(result.get(2, Long.class)
+                .intValue())
+            .build())
+        .toList();
+  }
+
 
 }
