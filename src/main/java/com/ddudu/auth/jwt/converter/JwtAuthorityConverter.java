61dc5f907cf4b2e4041564efaeea787ab0e60f94
Jay <injaesong0@gmail.com>
2023-12-30 02:29:26 +0900

    fix: 인증 토큰에 인가가 없을 경우 NPE 방지

--- src/main/java/com/ddudu/auth/jwt/converter/JwtAuthorityConverter.java
+++ src/main/java/com/ddudu/auth/jwt/converter/JwtAuthorityConverter.java
@@ -2,8 +2,10 @@ package com.ddudu.auth.jwt.converter;
 
 import com.ddudu.auth.domain.authority.Authority;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +17,11 @@ public class JwtAuthorityConverter implements Converter<Jwt, GrantedAuthority> {
   public GrantedAuthority convert(Jwt jwt) {
     String authorityName = jwt.getClaimAsString(AUTHORITY_CLAIM_NAME);
 
-    return Authority.valueOf(authorityName);
+    try {
+      return Authority.valueOf(authorityName);
+    } catch (NullPointerException e) {
+      throw new InvalidBearerTokenException(e.getMessage());
+    }
   }
 
 }
