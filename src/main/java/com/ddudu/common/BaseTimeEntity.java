546787b4819d31b266581d4bf98718d2ae472aa6
uijin-j <uijin0531@gmail.com>
2023-12-30 23:20:04 +0900

    refactor: BaseTimeEntity 생성

---
+++ src/main/java/com/ddudu/common/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.ddudu.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseTimeEntity {
+
+  @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP")
+  @CreatedDate
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMP")
+  @LastModifiedDate
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+  private LocalDateTime updatedAt;
+
+}
