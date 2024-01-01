3ef458fc57d72afea2243e1d90f2f6964c35643f
uijin-j <uijin0531@gmail.com>
2024-01-02 00:57:12 +0900

    feat: 할 일 생성 서비스 구현

--- src/main/java/com/ddudu/todo/service/TodoService.java
+++ src/main/java/com/ddudu/todo/service/TodoService.java
@@ -3,11 +3,15 @@ package com.ddudu.todo.service;
 import com.ddudu.goal.domain.Goal;
 import com.ddudu.goal.repository.GoalRepository;
 import com.ddudu.todo.domain.Todo;
+import com.ddudu.todo.dto.request.CreateTodoRequest;
 import com.ddudu.todo.dto.response.TodoInfo;
 import com.ddudu.todo.dto.response.TodoListResponse;
 import com.ddudu.todo.dto.response.TodoResponse;
 import com.ddudu.todo.repository.TodoRepository;
+import com.ddudu.user.domain.User;
+import com.ddudu.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Collections;
@@ -17,14 +21,34 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Validated
 public class TodoService {
 
   private final TodoRepository todoRepository;
   private final GoalRepository goalRepository;
+  private final UserRepository userRepository;
+
+  @Transactional
+  public TodoInfo create(Long userId, @Valid CreateTodoRequest request) {
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new EntityNotFoundException("해당 아이디를 가진 사용자가 존재하지 않습니다."));
+    Goal goal = goalRepository.findById(request.goalId())
+        .orElseThrow(() -> new EntityNotFoundException("해당 아이디를 가진 목표가 존재하지 않습니다."));
+
+    Todo todo = Todo.builder()
+        .name(request.name())
+        .goal(goal)
+        .user(user)
+        .beginAt(request.begin_at())
+        .build();
+
+    return TodoInfo.from(todoRepository.save(todo));
+  }
 
   public TodoResponse findById(Long id) {
     Todo todo = todoRepository.findById(id)
