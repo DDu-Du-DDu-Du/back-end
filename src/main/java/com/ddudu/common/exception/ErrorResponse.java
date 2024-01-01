0c5e81b954ab86ba630f212dfc2722b280a39db3
Jay <injaesong0@gmail.com>
2024-01-02 02:04:30 +0900

    refactor: 빌더 패턴 적용

--- src/main/java/com/ddudu/common/exception/ErrorResponse.java
+++ src/main/java/com/ddudu/common/exception/ErrorResponse.java
@@ -1,5 +1,8 @@
 package com.ddudu.common.exception;
 
+import lombok.Builder;
+
+@Builder
 public record ErrorResponse(int code, String message) {
 
   public static ErrorResponse from(BusinessException e) {
@@ -7,6 +10,9 @@ public record ErrorResponse(int code, String message) {
   }
 
   public static ErrorResponse from(int code, String message) {
-    return new ErrorResponse(code, message);
+    return ErrorResponse.builder()
+        .code(code)
+        .message(message)
+        .build();
   }
 }
