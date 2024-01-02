f3ea350d30511d2f7a61c8b56040679e91b725fd
Jay <injaesong0@gmail.com>
2024-01-02 17:25:13 +0900

    test: 비밀번호 최소 조건 충족

--- src/test/java/com/ddudu/todo/service/TodoServiceTest.java
+++ src/test/java/com/ddudu/todo/service/TodoServiceTest.java
@@ -99,7 +99,7 @@ class TodoServiceTest {
     String email = faker.internet()
         .emailAddress();
     String password = faker.internet()
-        .password(8, 40, false, true, true);
+        .password(8, 40, true, true, true);
     String nickname = faker.oscarMovie()
         .character();
 
