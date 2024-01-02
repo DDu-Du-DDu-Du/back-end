9b285d728fd15b38ac0373576e7c1344111db62c
EomYerim <djadpfa1002@naver.com>
2024-01-02 22:52:23 +0900

    feat: Todosoft delete로 인한 조회 조건(@SQLRestriction) 추가 TodoRepositoryImpl에서 where is_deleted = false 제거

--- src/main/java/com/ddudu/todo/domain/Todo.java
+++ src/main/java/com/ddudu/todo/domain/Todo.java
@@ -24,11 +24,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "todo")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@SQLRestriction("is_deleted = 0")
 public class Todo extends BaseEntity {
 
   private static final TodoStatus DEFAULT_STATUS = TodoStatus.UNCOMPLETED;
