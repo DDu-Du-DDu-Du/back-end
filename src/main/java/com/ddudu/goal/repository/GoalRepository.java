34fb0ff67e32a087a040fd18e4cbca0636e1db67
uijin-j <uijin0531@gmail.com>
2024-01-03 04:41:17 +0900

    refactor: QueryDSL 도입

--- src/main/java/com/ddudu/goal/repository/GoalRepository.java
+++ src/main/java/com/ddudu/goal/repository/GoalRepository.java
@@ -1,22 +1,15 @@
 package com.ddudu.goal.repository;
 
 import com.ddudu.goal.domain.Goal;
-import com.ddudu.user.domain.User;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface GoalRepository extends JpaRepository<Goal, Long> {
+public interface GoalRepository extends JpaRepository<Goal, Long>, GoalRepositoryCustom {
 
   @Query(
       "SELECT g FROM Goal g WHERE g.id=:id AND g.isDeleted=false"
   )
   Optional<Goal> findById(Long id);
-
-  @Query(
-      "SELECT g FROM Goal g WHERE g.user=:user AND g.isDeleted=false ORDER BY g.status DESC, g.createdAt ASC"
-  )
-  List<Goal> findAllByUser(User user);
 
 }
