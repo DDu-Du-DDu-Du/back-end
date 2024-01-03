225af8d052a7bf0e358b0d6f4a0856185c0846e7
Jay <injaesong0@gmail.com>
2024-01-04 04:40:16 +0900

    feat: 팔로잉 생성 시 토큰 파싱 후 사용자 로딩

--- src/test/java/com/ddudu/following/controller/FollowingControllerTest.java
+++ src/test/java/com/ddudu/following/controller/FollowingControllerTest.java
@@ -2,15 +2,20 @@ package com.ddudu.following.controller;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.ddudu.auth.domain.authority.Authority;
+import com.ddudu.common.exception.DataNotFoundException;
 import com.ddudu.config.JwtConfig;
 import com.ddudu.config.WebSecurityConfig;
 import com.ddudu.following.dto.request.FollowRequest;
 import com.ddudu.following.dto.response.FollowResponse;
+import com.ddudu.following.exception.FollowingErrorCode;
 import com.ddudu.following.service.FollowingService;
 import com.ddudu.support.TestProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,7 +29,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -44,9 +56,16 @@ class FollowingControllerTest {
   @Autowired
   MockMvc mockMvc;
 
+  @Autowired
+  JwtEncoder jwtEncoder;
+
   @Nested
   class POST_팔로잉_신청_API_테스트 {
 
+    static final JwsHeader header = JwsHeader.with(MacAlgorithm.HS512)
+        .build();
+    static final JwtClaimsSet.Builder claimSet = JwtClaimsSet.builder()
+        .claim("auth", Authority.NORMAL);
     Long followerId;
     Long followeeId;
 
@@ -59,17 +78,9 @@ class FollowingControllerTest {
     }
 
     @Test
-    void 팔로잉_신청을_성공한다() throws Exception {
+    void 로그인한_사용자가_없으면_401_Unauthorized를_반환한다() throws Exception {
       // given
-      FollowRequest request = new FollowRequest(followerId, followeeId);
-      FollowResponse response = FollowResponse.builder()
-          .id(1L)
-          .followerId(followerId)
-          .followeeId(followeeId)
-          .build();
-
-      given(followingService.create(any(FollowRequest.class)))
-          .willReturn(response);
+      FollowRequest request = new FollowRequest(followeeId);
 
       // when
       ResultActions actions = mockMvc.perform(post("/api/followings")
@@ -77,8 +88,62 @@ class FollowingControllerTest {
           .content(objectMapper.writeValueAsString(request)));
 
       // then
+      actions.andExpect(status().isUnauthorized())
+          .andExpect(header().string(HttpHeaders.WWW_AUTHENTICATE, "Bearer"));
+    }
+
+    @Test
+    void 팔로잉_대상_사용자가_존재하지_않으면_404_Not_Found를_반환한다() throws Exception {
+      // given
+      String token = createBearerToken(followerId);
+      FollowRequest request = new FollowRequest(followeeId);
+
+      given(followingService.create(anyLong(), any(FollowRequest.class)))
+          .willThrow(new DataNotFoundException(FollowingErrorCode.FOLLOWEE_NOT_EXISTING));
+
+      // when
+      ResultActions actions = mockMvc.perform(post("/api/followings")
+          .header("Authorization", token)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(objectMapper.writeValueAsString(request)));
+
+      // then
+      actions.andExpect(status().isNotFound())
+          .andExpect(jsonPath("$.code", is(FollowingErrorCode.FOLLOWEE_NOT_EXISTING.getCode())))
+          .andExpect(
+              jsonPath("$.message", is(FollowingErrorCode.FOLLOWEE_NOT_EXISTING.getMessage())));
+    }
+
+    @Test
+    void 팔로잉_신청을_성공한고_OK를_반환한다() throws Exception {
+      // given
+      String token = createBearerToken(followerId);
+      FollowRequest request = new FollowRequest(followeeId);
+      FollowResponse response = FollowResponse.builder()
+          .id(1L)
+          .followerId(followerId)
+          .followeeId(followeeId)
+          .build();
+
+      given(followingService.create(anyLong(), any(FollowRequest.class)))
+          .willReturn(response);
+
+      // when
+      ResultActions actions = mockMvc.perform(post("/api/followings")
+          .header("Authorization", token)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(objectMapper.writeValueAsString(request)));
+
+      // then
       actions.andExpect(status().isCreated())
           .andExpect(header().string("location", is("/api/followings/1")));
+    }
+
+    private String createBearerToken(long userId) {
+      JwtClaimsSet claims = claimSet.claim("user", userId)
+          .build();
+      Jwt jwt = jwtEncoder.encode(JwtEncoderParameters.from(header, claims));
+      return "Bearer " + jwt.getTokenValue();
     }
 
   }
