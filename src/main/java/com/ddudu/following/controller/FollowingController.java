5816032f6b24553b737388d7589927e7602d5610
Jay <injaesong0@gmail.com>
2024-01-04 13:56:18 +0900

    feat: 컨트롤러에서 팔로잉 생성 유효성 검증

--- src/main/java/com/ddudu/following/controller/FollowingController.java
+++ src/main/java/com/ddudu/following/controller/FollowingController.java
@@ -4,6 +4,7 @@ import com.ddudu.auth.jwt.JwtAuthToken;
 import com.ddudu.following.dto.request.FollowRequest;
 import com.ddudu.following.dto.response.FollowResponse;
 import com.ddudu.following.service.FollowingService;
+import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,7 @@ public class FollowingController {
   public ResponseEntity<FollowResponse> create(
       Authentication authentication,
       @RequestBody
+      @Valid
       FollowRequest request
   ) {
     long followerId = ((JwtAuthToken) authentication).getUserId();
