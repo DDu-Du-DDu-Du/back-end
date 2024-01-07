7f264a8d8e2ab0865480dc4c78ec15070331be41
EomYerim <djadpfa1002@naver.com>
2024-01-07 20:29:49 +0900

    feat: User controller 이메일 및 비밀번호 변경 추가

--- src/main/java/com/ddudu/user/controller/UserController.java
+++ src/main/java/com/ddudu/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ddudu.user.controller;
 
 import com.ddudu.auth.jwt.JwtAuthToken;
 import com.ddudu.user.dto.request.SignUpRequest;
+import com.ddudu.user.dto.request.UpdateEmailRequest;
+import com.ddudu.user.dto.request.UpdatePasswordRequest;
 import com.ddudu.user.dto.response.SignUpResponse;
 import com.ddudu.user.dto.response.UserResponse;
 import com.ddudu.user.service.UserService;
@@ -11,7 +13,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +25,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+  private static final String PASSWORD_UPDATE_SUCCESS = "비밀번호가 성공적으로 변경되었습니다.";
+
   private final UserService userService;
 
   @PostMapping
   public ResponseEntity<SignUpResponse> signUp(
       @RequestBody
       @Valid
-      SignUpRequest request
+          SignUpRequest request
   ) {
     SignUpResponse response = userService.signUp(request);
     URI uri = URI.create("/api/users/" + response.id());
@@ -42,6 +48,33 @@ public class UserController {
     UserResponse response = userService.findById(userId);
 
     return ResponseEntity.ok(response);
+  }
+
+  @PutMapping("/{id}/email")
+  public ResponseEntity<UserResponse> updateEmail(
+      @PathVariable
+          Long id,
+      @RequestBody
+      @Valid
+          UpdateEmailRequest request
+
+  ) {
+    UserResponse response = userService.updateEmail(id, request);
+
+    return ResponseEntity.ok(response);
+  }
+
+  @PutMapping("/{id}/password")
+  public ResponseEntity<String> updatePassword(
+      @PathVariable
+          Long id,
+      @RequestBody
+      @Valid
+          UpdatePasswordRequest request
+  ) {
+    userService.updatePassword(id, request);
+
+    return ResponseEntity.ok(PASSWORD_UPDATE_SUCCESS);
   }
 
 }
