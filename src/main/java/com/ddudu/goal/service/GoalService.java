ff6cd24a7a759bb9366951fc5a1416c6d90e2255
EomYerim <djadpfa1002@naver.com>
2024-01-08 02:51:14 +0900

    feat: Goal service 공개 설정 변경 추가

--- src/main/java/com/ddudu/goal/service/GoalService.java
+++ src/main/java/com/ddudu/goal/service/GoalService.java
@@ -4,6 +4,7 @@ import com.ddudu.common.exception.DataNotFoundException;
 import com.ddudu.goal.domain.Goal;
 import com.ddudu.goal.dto.requset.CreateGoalRequest;
 import com.ddudu.goal.dto.requset.UpdateGoalRequest;
+import com.ddudu.goal.dto.requset.UpdatePrivacyRequest;
 import com.ddudu.goal.dto.response.CreateGoalResponse;
 import com.ddudu.goal.dto.response.GoalResponse;
 import com.ddudu.goal.dto.response.GoalSummaryResponse;
@@ -11,7 +12,6 @@ import com.ddudu.goal.exception.GoalErrorCode;
 import com.ddudu.goal.repository.GoalRepository;
 import com.ddudu.user.domain.User;
 import com.ddudu.user.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -32,7 +32,7 @@ public class GoalService {
   public CreateGoalResponse create(
       Long userId,
       @Valid
-      CreateGoalRequest request
+          CreateGoalRequest request
   ) {
     User user = userRepository.findById(userId)
         .orElseThrow(() -> new DataNotFoundException(GoalErrorCode.USER_NOT_EXISTING));
@@ -80,6 +80,16 @@ public class GoalService {
   public void delete(Long id) {
     goalRepository.findById(id)
         .ifPresent(Goal::delete);
+  }
+
+  @Transactional
+  public GoalResponse updatePrivacy(Long id, @Valid UpdatePrivacyRequest request) {
+    Goal goal = goalRepository.findById(id)
+        .orElseThrow(() -> new DataNotFoundException(GoalErrorCode.ID_NOT_EXISTING));
+
+    goal.applyPrivacyUpdate(request.privacyType());
+
+    return GoalResponse.from(goal);
   }
 
 }
