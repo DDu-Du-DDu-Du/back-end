80a2fa6e7d6ecb10902df600666033e868bc49c6
EomYerim <djadpfa1002@naver.com>
2024-01-11 15:56:03 +0900

    feat: Like errorCode 추가 및 메세지 수정

--- src/main/java/com/ddudu/like/exception/LikeErrorCode.java
+++ src/main/java/com/ddudu/like/exception/LikeErrorCode.java
@@ -3,9 +3,14 @@ package com.ddudu.like.exception;
 import com.ddudu.common.exception.ErrorCode;
 
 public enum LikeErrorCode implements ErrorCode {
-  NULL_USER(7001, "사용자가 반드시 존재해야 합니다."),
-  NULL_TODO(7002, "좋아요할 할 일이 반드시 존재해야 합니다."),
-  SELF_LIKE_UNAVAILABLE(7003, "본인의 할 일에 좋아요를 할 수 없습니다.");
+  NULL_USER(7001, "사용자는 반드시 존재해야 합니다."),
+  NULL_TODO(7002, "좋아요할 할 일은 반드시 존재해야 합니다."),
+  SELF_LIKE_UNAVAILABLE(7003, "본인의 할 일에는 좋아요를 할 수 없습니다."),
+  USER_NOT_EXISTING(7004, "좋아요를 보내려는 사용자를 찾을 수 없습니다."),
+  TODO_NOT_EXISTING(7005, "좋아요를 보내려는 할 일을 찾을 수 없습니다."),
+  ALREADY_LIKED(7006, "이미 좋아요를 눌렀습니다."),
+  UNAVAILABLE_UNCOMPLETED_TODO(7007, "완료한 다음에 좋아요를 보낼 수 있습니다"),
+  INVALID_AUTHORITY(7008, "해당 기능에 대한 사용자 권한이 없습니다.");
 
   private final int code;
   private final String message;
