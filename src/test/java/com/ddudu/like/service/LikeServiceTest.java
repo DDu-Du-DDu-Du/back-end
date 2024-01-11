4142f1b663eb83f329aaf4c5f369819808f6c3c4
EomYerim <djadpfa1002@naver.com>
2024-01-11 16:01:02 +0900

    test: Like 좋아요 생성 관련 서비스 테스트 코드 작성

---
+++ src/test/java/com/ddudu/like/service/LikeServiceTest.java
@@ -0,0 +1,199 @@
+package com.ddudu.like.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import com.ddudu.common.exception.DataNotFoundException;
+import com.ddudu.common.exception.DuplicateResourceException;
+import com.ddudu.common.exception.ForbiddenException;
+import com.ddudu.goal.domain.Goal;
+import com.ddudu.goal.domain.PrivacyType;
+import com.ddudu.goal.repository.GoalRepository;
+import com.ddudu.like.domain.Like;
+import com.ddudu.like.dto.request.LikeRequest;
+import com.ddudu.like.dto.response.LikeResponse;
+import com.ddudu.like.exception.LikeErrorCode;
+import com.ddudu.like.repository.LikeRepository;
+import com.ddudu.todo.domain.Todo;
+import com.ddudu.todo.repository.TodoRepository;
+import com.ddudu.user.domain.User;
+import com.ddudu.user.repository.UserRepository;
+import java.util.Optional;
+import net.datafaker.Faker;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class LikeServiceTest {
+
+  static final Faker faker = new Faker();
+
+  @Autowired
+  LikeService likeService;
+
+  @Autowired
+  LikeRepository likeRepository;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  GoalRepository goalRepository;
+
+  @Autowired
+  TodoRepository todoRepository;
+
+  User user;
+  Goal goal;
+  Todo todo;
+
+  @BeforeEach
+  void setUp() {
+    user = createUser();
+    goal = createGoal(user);
+    todo = createTodo(user, goal);
+    todo.switchStatus();
+  }
+
+  @Nested
+  class 좋아요_생성_테스트 {
+
+    @Test
+    void 로그인한_사용자가_좋아요할_사용자와_다르면_좋아요_생성을_실패한다() {
+      // given
+      User other = createUser();
+      LikeRequest request = new LikeRequest(user.getId(), todo.getId());
+
+      // when
+      ThrowingCallable create = () -> likeService.create(other.getId(), request);
+
+      // then
+      assertThatExceptionOfType(ForbiddenException.class).isThrownBy(create)
+          .withMessage(LikeErrorCode.INVALID_AUTHORITY.getMessage());
+    }
+
+    @Test
+    void 사용자_아이디가_존재하지_않으면_좋아요_생성을_실패한다() {
+      // given
+      Long invalidUserId = faker.random()
+          .nextLong(Long.MAX_VALUE);
+      LikeRequest request = new LikeRequest(invalidUserId, todo.getId());
+
+      // when
+      ThrowingCallable create = () -> likeService.create(invalidUserId, request);
+
+      // then
+      assertThatExceptionOfType(DataNotFoundException.class).isThrownBy(create)
+          .withMessage(LikeErrorCode.USER_NOT_EXISTING.getMessage());
+    }
+
+    @Test
+    void 할_일_아이디가_존재하지_않으면_좋아요_생성을_실패한다() {
+      // given
+      User other = createUser();
+      Long invalidTodoId = faker.random()
+          .nextLong(Long.MAX_VALUE);
+      LikeRequest request = new LikeRequest(other.getId(), invalidTodoId);
+
+      // when
+      ThrowingCallable create = () -> likeService.create(other.getId(), request);
+
+      // then
+      assertThatExceptionOfType(DataNotFoundException.class).isThrownBy(create)
+          .withMessage(LikeErrorCode.TODO_NOT_EXISTING.getMessage());
+    }
+
+    @Test
+    void 이미_좋아요를_눌렀다면_좋아요_생성을_실패한다() {
+      // given
+      User other = createUser();
+
+      Like like = Like.builder()
+          .user(other)
+          .todo(todo)
+          .build();
+      likeRepository.save(like);
+
+      LikeRequest request = new LikeRequest(other.getId(), todo.getId());
+
+      // when
+      ThrowingCallable create = () -> likeService.create(other.getId(), request);
+
+      // then
+      assertThatExceptionOfType(DuplicateResourceException.class).isThrownBy(create)
+          .withMessage(LikeErrorCode.ALREADY_LIKED.getMessage());
+    }
+
+    @Test
+    void 완료된_할_일에_대해_좋아요_생성을_성공한다() {
+      // given
+      User other = createUser();
+      LikeRequest request = new LikeRequest(other.getId(), todo.getId());
+
+      // when
+      LikeResponse expected = likeService.create(other.getId(), request);
+
+      // then
+      Optional<Like> actual = likeRepository.findById(expected.id());
+      assertThat(actual).isPresent();
+      assertThat(actual.get()).extracting("user", "todo")
+          .containsExactly(other, todo);
+    }
+
+  }
+
+  private User createUser() {
+    String email = faker.internet()
+        .emailAddress();
+    String password = faker.internet()
+        .password(8, 40, true, true, true);
+    String nickname = faker.oscarMovie()
+        .character();
+
+    User user = User.builder()
+        .passwordEncoder(new BCryptPasswordEncoder())
+        .email(email)
+        .password(password)
+        .nickname(nickname)
+        .build();
+
+    return userRepository.save(user);
+  }
+
+  private Goal createGoal(User user) {
+    String name = faker.lorem()
+        .word();
+
+    Goal goal = Goal.builder()
+        .name(name)
+        .user(user)
+        .privacyType(PrivacyType.PUBLIC)
+        .build();
+
+    return goalRepository.save(goal);
+  }
+
+  private Todo createTodo(User user, Goal goal) {
+    String name = faker.lorem()
+        .word();
+
+    Todo todo = Todo.builder()
+        .name(name)
+        .user(user)
+        .goal(goal)
+        .build();
+
+    return todoRepository.save(todo);
+  }
+
+}
