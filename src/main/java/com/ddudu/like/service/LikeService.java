5b5e4e4408e6fae6f9af64344f9265823716f884
EomYerim <djadpfa1002@naver.com>
2024-01-13 00:13:29 +0900

    refactor: Like service 기존 생성, 삭제 제거 및 toggle 방식으로 변경

--- src/main/java/com/ddudu/like/service/LikeService.java
+++ src/main/java/com/ddudu/like/service/LikeService.java
@@ -1,7 +1,6 @@
 package com.ddudu.like.service;
 
 import com.ddudu.common.exception.DataNotFoundException;
-import com.ddudu.common.exception.DuplicateResourceException;
 import com.ddudu.common.exception.ForbiddenException;
 import com.ddudu.like.domain.Like;
 import com.ddudu.like.dto.request.LikeRequest;
@@ -12,24 +11,24 @@ import com.ddudu.todo.domain.Todo;
 import com.ddudu.todo.repository.TodoRepository;
 import com.ddudu.user.domain.User;
 import com.ddudu.user.repository.UserRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class LikeService {
 
+  private static final String SUCCESS = "좋아요";
+  private static final String CANCEL = "좋아요 취소";
+
   private final LikeRepository likeRepository;
   private final UserRepository userRepository;
   private final TodoRepository todoRepository;
 
   @Transactional
-  @Validated
-  public LikeResponse create(Long loginId, @Valid LikeRequest request) {
+  public LikeResponse toggle(Long loginId, LikeRequest request) {
     checkPermission(loginId, request.userId());
 
     User user = userRepository.findById(request.userId())
@@ -37,8 +36,10 @@ public class LikeService {
     Todo todo = todoRepository.findById(request.todoId())
         .orElseThrow(() -> new DataNotFoundException(LikeErrorCode.TODO_NOT_EXISTING));
 
-    if (likeRepository.existsByUserAndTodo(user, todo)) {
-      throw new DuplicateResourceException(LikeErrorCode.ALREADY_LIKED);
+    Like existingLike = likeRepository.findByUserAndTodo(user, todo);
+
+    if (existingLike != null) {
+      return LikeResponse.from(existingLike, switchLikeStatus(existingLike));
     }
 
     Like like = Like.builder()
@@ -46,23 +47,23 @@ public class LikeService {
         .todo(todo)
         .build();
 
-    return LikeResponse.from(likeRepository.save(like));
-  }
-
-  @Transactional
-  public void delete(Long loginId, Long id) {
-    likeRepository.findById(id)
-        .ifPresent(like -> {
-          checkPermission(loginId, like.getUser()
-              .getId());
-          like.delete();
-        });
+    return LikeResponse.from(likeRepository.save(like), SUCCESS);
   }
 
   private void checkPermission(Long loginId, Long userId) {
     if (!loginId.equals(userId)) {
       throw new ForbiddenException(LikeErrorCode.INVALID_AUTHORITY);
     }
+  }
+
+  private String switchLikeStatus(Like like) {
+    if (!like.isDeleted()) {
+      like.delete();
+      return CANCEL;
+    }
+
+    like.undelete();
+    return SUCCESS;
   }
 
 }
