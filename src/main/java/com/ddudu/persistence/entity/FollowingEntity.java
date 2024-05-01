1b958ca071b98bc81761cc8bb2fdd952f3574f9c
uijin-j <uijin0531@gmail.com>
2024-05-01 14:17:33 +0900

    refactor: Following 도메인/엔티티 분리

---
+++ src/main/java/com/ddudu/persistence/entity/FollowingEntity.java
@@ -0,0 +1,81 @@
+package com.ddudu.persistence.entity;
+
+import com.ddudu.common.BaseEntity;
+import com.ddudu.user.domain.Following;
+import com.ddudu.user.domain.FollowingStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "followings")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FollowingEntity extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "follower_id", referencedColumnName = "id", nullable = false)
+  private UserEntity follower;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "followee_id", referencedColumnName = "id", nullable = false)
+  private UserEntity followee;
+
+  @Column(name = "status", nullable = false, columnDefinition = "VARCHAR", length = 20)
+  @Enumerated(EnumType.STRING)
+  private FollowingStatus status;
+
+  @Builder
+  public FollowingEntity(
+      Long id, UserEntity follower, UserEntity followee, FollowingStatus status,
+      LocalDateTime createdAt, LocalDateTime updatedAt, Boolean isDeleted
+  ) {
+    super(createdAt, updatedAt, isDeleted);
+
+    this.id = id;
+    this.follower = follower;
+    this.followee = followee;
+    this.status = status;
+  }
+
+  public static FollowingEntity from(Following following) {
+    return FollowingEntity.builder()
+        .id(following.getId())
+        .follower(UserEntity.from(following.getFollower()))
+        .followee(UserEntity.from(following.getFollowee()))
+        .status(following.getStatus())
+        .createdAt(following.getCreatedAt())
+        .updatedAt(following.getUpdatedAt())
+        .isDeleted(following.isDeleted())
+        .build();
+  }
+
+  public Following toDomain() {
+    return Following.builder()
+        .id(id)
+        .follower(follower.toDomain())
+        .followee(followee.toDomain())
+        .status(status)
+        .createdAt(getCreatedAt())
+        .updatedAt(getUpdatedAt())
+        .isDeleted(isDeleted())
+        .build();
+  }
+
+}
--- src/main/java/com/ddudu/user/domain/Following.java
+++ src/main/java/com/ddudu/user/domain/Following.java
@@ -1,59 +1,37 @@
 package com.ddudu.user.domain;
 
-import com.ddudu.common.BaseEntity;
+import com.ddudu.common.domain.BaseDomain;
 import com.ddudu.common.exception.InvalidParameterException;
 import com.ddudu.user.exception.FollowingErrorCode;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.Objects;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "followings")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Following extends BaseEntity {
+public class Following extends BaseDomain {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
   private Long id;
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "follower_id", referencedColumnName = "id", nullable = false)
   private User follower;
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "followee_id", referencedColumnName = "id", nullable = false)
   private User followee;
-
-  @Column(name = "status", nullable = false, columnDefinition = "VARCHAR", length = 20)
-  @Enumerated(EnumType.STRING)
   private FollowingStatus status;
 
   @Builder
-  public Following(User follower, User followee, FollowingStatus status) {
+  public Following(
+      Long id, User follower, User followee, FollowingStatus status,
+      LocalDateTime createdAt, LocalDateTime updatedAt, Boolean isDeleted
+  ) {
+    super(createdAt, updatedAt, isDeleted);
     validate(follower, followee);
 
+    this.id = id;
     this.follower = follower;
     this.followee = followee;
     this.status = Objects.requireNonNullElse(status, FollowingStatus.FOLLOWING);
   }
 
   public boolean isRequestedTo(User user) {
-    return  Objects.equals(followee, user);
+    return Objects.equals(followee, user);
   }
 
   public boolean isOwnedBy(User owner) {
