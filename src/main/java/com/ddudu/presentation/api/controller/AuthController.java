d96b57d0b5dc544d19737626dd3055bc209eb21a
Jay <injaesong0@gmail.com>
2024-05-07 19:14:17 +0900

    refactor: 로그인 컨트롤러 변경 반영

--- src/main/java/com/ddudu/presentation/api/controller/AuthController.java
+++ src/main/java/com/ddudu/presentation/api/controller/AuthController.java
@@ -1,22 +1,25 @@
 package com.ddudu.presentation.api.controller;
 
-import com.ddudu.old.auth.dto.request.LoginRequest;
-import com.ddudu.old.auth.dto.response.LoginResponse;
+import com.ddudu.application.domain.authentication.dto.request.SocialRequest;
+import com.ddudu.application.domain.authentication.dto.response.TokenResponse;
+import com.ddudu.application.port.in.SocialLoginUseCase;
 import com.ddudu.old.auth.dto.response.MeResponse;
 import com.ddudu.old.auth.service.AuthService;
 import com.ddudu.presentation.api.annotation.Login;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,24 +29,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "인증 관련 API")
 public class AuthController {
 
+  private final SocialLoginUseCase socialLoginUseCase;
   private final AuthService authService;
 
-  @PostMapping("/login")
-  @Operation(summary = "로그인")
+  @PostMapping("/login/{providerType}")
+  @Operation(summary = "소셜 로그인")
   @ApiResponse(
       responseCode = "200",
       content = @Content(
           mediaType = MediaType.APPLICATION_JSON_VALUE,
-          schema = @Schema(implementation = LoginResponse.class)
+          schema = @Schema(implementation = TokenResponse.class)
       )
   )
-  @Deprecated
-  public ResponseEntity<LoginResponse> login(
-      @RequestBody
-      @Valid
-      LoginRequest request
+  public ResponseEntity<TokenResponse> login(
+      @RequestHeader(HttpHeaders.AUTHORIZATION)
+      @Parameter(hidden = true)
+      String socialToken,
+      @PathVariable
+      String providerType
   ) {
-    LoginResponse response = authService.login(request);
+    SocialRequest request = new SocialRequest(socialToken, providerType);
+    TokenResponse response = socialLoginUseCase.login(request);
 
     return ResponseEntity.ok(response);
   }
