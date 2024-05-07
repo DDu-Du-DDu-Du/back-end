033e32efd43d65321877141b940e0662d4227fa0
Jay <injaesong0@gmail.com>
2024-05-07 19:06:18 +0900

    feat: 외부 api http 호출 로직 구현

---
+++ src/main/java/com/ddudu/infrastructure/api/operator/HttpOperator.java
@@ -0,0 +1,32 @@
+package com.ddudu.infrastructure.api.operator;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
+public class HttpOperator {
+
+  private final RestTemplate restTemplate;
+
+  public ResponseEntity<String> operateExchangeWithGetMethod(
+      String accessToken, String destination
+  ) {
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.setBearerAuth(accessToken);
+    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+    HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
+
+    return restTemplate.exchange(destination, HttpMethod.GET, request, String.class);
+  }
+
+}
