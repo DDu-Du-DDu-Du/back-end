a2fe66250984dfbfaa28f29b24aeace025dbd340
Jay <injaesong0@gmail.com>
2024-05-07 19:08:26 +0900

    refactor: 마이그레이션으로 인한 불필요 코드 제거

--- src/main/java/com/ddudu/old/auth/service/AuthService.java
+++ src/main/java/com/ddudu/old/auth/service/AuthService.java
@@ -1,24 +1,12 @@
 package com.ddudu.old.auth.service;
 
-import com.ddudu.old.auth.dto.request.LoginRequest;
-import com.ddudu.old.auth.dto.response.LoginResponse;
-import com.ddudu.old.auth.dto.response.MeResponse;
-import com.ddudu.old.auth.exception.AuthErrorCode;
-import com.ddudu.old.auth.jwt.JwtIssuer;
-import com.ddudu.presentation.api.exception.BadCredentialsException;
-import com.ddudu.presentation.api.exception.DataNotFoundException;
-import com.ddudu.presentation.api.exception.InvalidTokenException;
-import com.ddudu.old.config.properties.JwtProperties;
-import com.ddudu.application.domain.user.domain.Email;
-import com.ddudu.application.domain.user.domain.Password;
+import com.ddudu.application.domain.authentication.exception.AuthErrorCode;
 import com.ddudu.application.domain.user.domain.User;
+import com.ddudu.old.auth.dto.response.MeResponse;
 import com.ddudu.old.user.domain.UserRepository;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import com.ddudu.presentation.api.exception.InvalidTokenException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,30 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
 
   private final UserRepository userRepository;
-  private final PasswordEncoder passwordEncoder;
-  private final JwtIssuer jwtIssuer;
-  private final JwtProperties jwtProperties;
-
-  public LoginResponse login(LoginRequest request) {
-    Email email = new Email(request.email());
-
-    User user = userRepository.findByEmail(email.getAddress())
-        .orElseThrow(() -> new DataNotFoundException(AuthErrorCode.EMAIL_NOT_EXISTING));
-    Password userPassword = user.getPassword();
-
-    if (!userPassword.check(request.password(), passwordEncoder)) {
-      throw new BadCredentialsException(AuthErrorCode.BAD_CREDENTIALS);
-    }
-
-    Map<String, Object> claims = new HashMap<>();
-
-    claims.put("user", user.getId());
-    claims.put("auth", user.getAuthority());
-
-    String jwt = jwtIssuer.issue(claims, Duration.ofMinutes(jwtProperties.getExpiredAfter()));
-
-    return new LoginResponse(jwt);
-  }
 
   public MeResponse loadUser(Long loginId) {
     User user = userRepository.findById(loginId)
--- src/main/java/com/ddudu/presentation/api/config/WebSecurityConfig.java
+++ src/main/java/com/ddudu/presentation/api/config/WebSecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,11 +38,6 @@ public class WebSecurityConfig {
             .jwt(jwt -> jwt
                 .jwtAuthenticationConverter(jwtConverter)))
         .build();
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
   }
 
 }
