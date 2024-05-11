353472406f209741b6f4f3db62ad1abb3112bfc1
uijin-j <uijin0531@gmail.com>
2024-05-12 03:47:26 +0900

    feat: GoalErrorCode에 NULL_USER 예외 추가

--- src/main/java/com/ddudu/application/domain/goal/exception/GoalErrorCode.java
+++ src/main/java/com/ddudu/application/domain/goal/exception/GoalErrorCode.java
@@ -16,7 +16,8 @@ public enum GoalErrorCode implements ErrorCode {
   NULL_PRIVACY_TYPE(3007, "공개 설정이 입력되지 않았습니다."),
   USER_NOT_EXISTING(3008, "해당 아이디를 가진 사용자가 존재하지 않습니다."),
   INVALID_AUTHORITY(3009, "해당 기능에 대한 사용자 권한이 없습니다."),
-  INVALID_PRIVACY_TYPE(3010, "존재하지 않은 공개 설정입니다.");
+  INVALID_PRIVACY_TYPE(3010, "존재하지 않은 공개 설정입니다."),
+  NULL_USER(3011, "사용자는 필수값입니다.");
 
   private final int code;
   private final String message;
