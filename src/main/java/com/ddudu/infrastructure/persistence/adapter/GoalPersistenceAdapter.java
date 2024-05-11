08b54916906d51922af737514bfa885e037d8ddb
uijin-j <uijin0531@gmail.com>
2024-05-12 03:46:39 +0900

    feat: GoalLoaderPort 구현

--- src/main/java/com/ddudu/infrastructure/persistence/adapter/GoalPersistenceAdapter.java
+++ src/main/java/com/ddudu/infrastructure/persistence/adapter/GoalPersistenceAdapter.java
@@ -1,22 +1,30 @@
 package com.ddudu.infrastructure.persistence.adapter;
 
 import com.ddudu.application.domain.goal.domain.Goal;
+import com.ddudu.application.port.out.GoalLoaderPort;
 import com.ddudu.application.port.out.SaveGoalPort;
 import com.ddudu.infrastructure.annotation.DrivenAdapter;
 import com.ddudu.infrastructure.persistence.entity.GoalEntity;
 import com.ddudu.infrastructure.persistence.repository.goal.GoalRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @DrivenAdapter
 @RequiredArgsConstructor
-public class GoalPersistenceAdapter implements SaveGoalPort {
+public class GoalPersistenceAdapter implements SaveGoalPort, GoalLoaderPort {
 
-  private GoalRepository goalRepository;
+  private final GoalRepository goalRepository;
 
   @Override
   public Goal save(Goal goal) {
     return goalRepository.save(GoalEntity.from(goal))
         .toDomain();
+  }
+
+  @Override
+  public Optional<Goal> findById(Long id) {
+    return goalRepository.findById(id)
+        .map(GoalEntity::toDomain);
   }
 
 }
