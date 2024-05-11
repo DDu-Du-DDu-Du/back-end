d28f4a9045c333db569a260e9c78bd874ba2e056
uijin-j <uijin0531@gmail.com>
2024-05-12 03:59:10 +0900

    style: import 순서 변경

--- src/test/java/com/ddudu/old/like/domain/LikeTest.java
+++ src/test/java/com/ddudu/old/like/domain/LikeTest.java
@@ -3,8 +3,8 @@ package com.ddudu.old.like.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import com.ddudu.application.domain.user.domain.User;
 import com.ddudu.application.domain.goal.domain.Goal;
+import com.ddudu.application.domain.user.domain.User;
 import com.ddudu.old.like.exception.LikeErrorCode;
 import com.ddudu.old.todo.domain.Todo;
 import com.ddudu.presentation.api.exception.InvalidParameterException;
