39c5a009e36b1279f5b898ab0e81437acf01949c
uijin-j <uijin0531@gmail.com>
2024-05-12 19:17:18 +0900

    feart: 목표 전체 조회 비즈니스 로직 구현

--- src/main/java/com/ddudu/application/service/RetrieveAllGoalsService.java
+++ src/main/java/com/ddudu/application/service/RetrieveAllGoalsService.java
@@ -2,7 +2,12 @@ package com.ddudu.application.service;
 
 import com.ddudu.application.annotation.UseCase;
 import com.ddudu.application.domain.goal.dto.response.GoalSummaryResponse;
+import com.ddudu.application.domain.goal.exception.GoalErrorCode;
+import com.ddudu.application.domain.user.domain.User;
 import com.ddudu.application.port.in.RetrieveAllGoalsUseCase;
+import com.ddudu.application.port.out.GoalLoaderPort;
+import com.ddudu.application.port.out.UserLoaderPort;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +17,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class RetrieveAllGoalsService implements RetrieveAllGoalsUseCase {
 
+  private final UserLoaderPort userLoaderPort;
+  private final GoalLoaderPort goalLoaderPort;
+
   @Override
   public List<GoalSummaryResponse> findAllByUser(Long userId) {
-    // TODO: 사용자 조회
-    // TODO: 사용자의 목표 조회
-    // TODO: DTO로 변환 및 반환
+    User user = findUser(userId);
 
-    return null;
+    return goalLoaderPort.findAllByUser(user)
+        .stream()
+        .map(GoalSummaryResponse::from)
+        .toList();
+  }
+
+  private User findUser(Long userId) {
+    return userLoaderPort.findById(userId)
+        .orElseThrow(
+            () -> new EntityNotFoundException(GoalErrorCode.USER_NOT_EXISTING.getCodeName()));
   }
 
 }
