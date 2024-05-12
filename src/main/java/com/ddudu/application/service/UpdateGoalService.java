4bbb0c6404b4e5f3893bbdb00d371a3f6fbe1e04
uijin-j <uijin0531@gmail.com>
2024-05-13 03:01:14 +0900

    feat: 목표 수정 비즈니스 로직 구현

--- src/main/java/com/ddudu/application/service/UpdateGoalService.java
+++ src/main/java/com/ddudu/application/service/UpdateGoalService.java
@@ -1,10 +1,18 @@
 package com.ddudu.application.service;
 
 import com.ddudu.application.annotation.UseCase;
+import com.ddudu.application.domain.goal.domain.Goal;
 import com.ddudu.application.domain.goal.dto.request.UpdateGoalRequest;
 import com.ddudu.application.domain.goal.dto.response.GoalResponse;
+import com.ddudu.application.domain.goal.exception.GoalErrorCode;
+import com.ddudu.application.domain.user.domain.User;
 import com.ddudu.application.port.in.UpdateGoalUseCase;
+import com.ddudu.application.port.out.GoalLoaderPort;
+import com.ddudu.application.port.out.UpdateGoalPort;
+import com.ddudu.application.port.out.UserLoaderPort;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.transaction.annotation.Transactional;
 
 @UseCase
@@ -12,13 +20,38 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UpdateGoalService implements UpdateGoalUseCase {
 
+  private final UserLoaderPort userLoaderPort;
+  private final GoalLoaderPort goalLoaderPort;
+  private final UpdateGoalPort updateGoalPort;
+
   @Override
   public GoalResponse update(Long userId, Long id, UpdateGoalRequest request) {
-    // TODO: 사용자 조회
-    // TODO: 목표 조회
-    // TODO: 사용자가 목표의 소유자인지 확인
-    // TODO: 목표 업데이트
-    return null;
+    User user = findUser(userId);
+    Goal goal = findGoal(id);
+
+    checkAuthority(user, goal);
+
+    goal.applyGoalUpdates(request.name(), request.status(), request.color(), request.privacyType());
+
+    return GoalResponse.from(updateGoalPort.update(goal));
+  }
+
+  private User findUser(Long userId) {
+    return userLoaderPort.findById(userId)
+        .orElseThrow(
+            () -> new EntityNotFoundException(GoalErrorCode.USER_NOT_EXISTING.getCodeName()));
+  }
+
+  private Goal findGoal(Long id) {
+    return goalLoaderPort.findById(id)
+        .orElseThrow(
+            () -> new EntityNotFoundException(GoalErrorCode.ID_NOT_EXISTING.getCodeName()));
+  }
+
+  private void checkAuthority(User user, Goal goal) {
+    if (!goal.isCreatedBy(user.getId())) {
+      throw new AccessDeniedException(GoalErrorCode.INVALID_AUTHORITY.getCodeName());
+    }
   }
 
 }
