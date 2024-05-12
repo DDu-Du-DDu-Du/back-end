2c61a4de9d84d3f0c04d71bd31968867a6c0a139
uijin-j <uijin0531@gmail.com>
2024-05-13 00:33:05 +0900

    feat: 단일 목표 조회 비즈니스 로직 구현

--- src/main/java/com/ddudu/application/service/RetrieveGoalService.java
+++ src/main/java/com/ddudu/application/service/RetrieveGoalService.java
@@ -1,8 +1,14 @@
 package com.ddudu.application.service;
 
 import com.ddudu.application.annotation.UseCase;
+import com.ddudu.application.domain.goal.domain.Goal;
 import com.ddudu.application.domain.goal.dto.response.GoalResponse;
+import com.ddudu.application.domain.goal.exception.GoalErrorCode;
+import com.ddudu.application.domain.user.domain.User;
 import com.ddudu.application.port.in.RetrieveGoalUseCase;
+import com.ddudu.application.port.out.GoalLoaderPort;
+import com.ddudu.application.port.out.UserLoaderPort;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,13 +17,35 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class RetrieveGoalService implements RetrieveGoalUseCase {
 
+  private final UserLoaderPort userLoaderPort;
+  private final GoalLoaderPort goalLoaderPort;
+
   @Override
   public GoalResponse getById(Long userId, Long id) {
-    // TODO: 사용자 조회
-    // TODO: 목표 조회
-    // TODO: 권한 검사
-    // TODO: 응답 생성
-    return null;
+    User user = findUser(userId);
+    Goal goal = findGoal(id);
+
+    checkAuthority(user, goal);
+
+    return GoalResponse.from(goal);
+  }
+
+  private User findUser(Long userId) {
+    return userLoaderPort.findById(userId)
+        .orElseThrow(
+            () -> new EntityNotFoundException(GoalErrorCode.USER_NOT_EXISTING.getCodeName()));
+  }
+
+  private Goal findGoal(Long id) {
+    return goalLoaderPort.findById(id)
+        .orElseThrow(
+            () -> new EntityNotFoundException(GoalErrorCode.ID_NOT_EXISTING.getCodeName()));
+  }
+
+  private void checkAuthority(User user, Goal goal) {
+    if (!goal.isCreatedBy(user.getId())) {
+      throw new EntityNotFoundException(GoalErrorCode.INVALID_AUTHORITY.getCodeName());
+    }
   }
 
 }
