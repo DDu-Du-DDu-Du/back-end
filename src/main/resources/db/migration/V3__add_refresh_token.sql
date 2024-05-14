e41fd08f939a07189315d60fc3242af822a3c6e9
Jay <injaesong0@gmail.com>
2024-05-14 12:52:02 +0900

    feat: 리프레시 토큰 복합키 DDL 추가

--- src/main/resources/db/migration/V3__add_refresh_token.sql
+++ src/main/resources/db/migration/V3__add_refresh_token.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS refresh_tokens
 (
-    token_value VARCHAR(255) NOT NULL,
     user_id     BIGINT       NOT NULL,
     family      INT          NOT NULL,
+    token_value VARCHAR(255) NOT NULL,
     created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT pk_refresh_token_id PRIMARY KEY (token_value)
+    CONSTRAINT pk_refresh_token_id PRIMARY KEY (user_id, family),
+    CONSTRAINT fk_refresh_token_user_id FOREIGN KEY (user_id) REFERENCES users (id)
 );
