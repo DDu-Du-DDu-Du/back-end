63f627c2c0c7ad3209cb3c8e42bd19ed4c377f64
Jay <injaesong0@gmail.com>
2024-05-15 23:21:36 +0900

    feat: 유저 패밀리 객체 빌더 추가

--- src/main/java/com/ddudu/application/domain/authentication/domain/vo/UserFamily.java
+++ src/main/java/com/ddudu/application/domain/authentication/domain/vo/UserFamily.java
@@ -3,18 +3,33 @@ package com.ddudu.application.domain.authentication.domain.vo;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import com.ddudu.application.domain.authentication.exception.AuthErrorCode;
+import java.util.Objects;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public class UserFamily {
 
   private final Long userId;
   private final int family;
 
   @Builder
+  private UserFamily(Long userId, int family) {
+    checkArgument(
+        Objects.nonNull(userId), AuthErrorCode.INVALID_USER_ID_FOR_REFRESH_TOKEN.getCodeName());
+
+    this.userId = userId;
+    this.family = family;
+  }
+
+  @Builder(
+      builderMethodName = "builderWithString",
+      buildMethodName = "buildWithString"
+  )
   private UserFamily(String userFamilyValue) {
-    String[] userFamily = userFamilyValue.split(" ");
+    String[] userFamily = userFamilyValue.split("-");
 
     checkArgument(
         userFamily.length == 2, AuthErrorCode.UNABLE_TO_PARSE_USER_FAMILY_VALUE.getCodeName());
@@ -23,12 +38,16 @@ public class UserFamily {
     this.family = getFamily(userFamily[1]);
   }
 
+  public String getUserFamilyValue() {
+    return userId + "-" + family;
+  }
+
   private Long getUserId(String userIdValue) {
     try {
       return Long.parseLong(userIdValue);
     } catch (RuntimeException e) {
       throw new IllegalArgumentException(
-          AuthErrorCode.INVALID_USER_ID_FOR_REFRESH_TOKEN.getCodeName());
+          AuthErrorCode.INVALID_USER_ID_FOR_REFRESH_TOKEN.getCodeName(), e);
     }
   }
 
@@ -36,7 +55,8 @@ public class UserFamily {
     try {
       return Integer.parseInt(familyValue);
     } catch (RuntimeException e) {
-      throw new IllegalArgumentException(AuthErrorCode.INVALID_REFRESH_TOKEN_FAMILY.getCodeName());
+      throw new IllegalArgumentException(
+          AuthErrorCode.INVALID_REFRESH_TOKEN_FAMILY.getCodeName(), e);
     }
   }
 
