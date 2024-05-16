7fdb2f28702ac572c8dc415d03d66b589e84aa9c
Jay <injaesong0@gmail.com>
2024-05-16 13:00:37 +0900

    refactor: 리프레시 토큰 참조 무결성 제거

--- src/main/resources/db/migration/V3__add_refresh_token.sql
+++ src/main/resources/db/migration/V3__add_refresh_token.sql
@@ -6,6 +6,5 @@ CREATE TABLE IF NOT EXISTS refresh_tokens
     token_value VARCHAR(255) NOT NULL,
     created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT pk_refresh_token_id PRIMARY KEY (id),
-    CONSTRAINT fk_refresh_token_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+    CONSTRAINT pk_refresh_token_id PRIMARY KEY (id)
 );
