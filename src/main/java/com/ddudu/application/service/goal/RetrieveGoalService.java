bef2828c6020753c410a1c16d898b90f67ec1ddd
uijin-j <uijin0531@gmail.com>
2024-05-16 18:16:35 +0900

    refactor: 불필요한 의존성 제거

--- src/main/java/com/ddudu/application/service/goal/RetrieveGoalService.java
+++ src/main/java/com/ddudu/application/service/goal/RetrieveGoalService.java
@@ -6,7 +6,6 @@ import com.ddudu.application.domain.goal.dto.response.GoalResponse;
 import com.ddudu.application.domain.goal.exception.GoalErrorCode;
 import com.ddudu.application.port.in.goal.RetrieveGoalUseCase;
 import com.ddudu.application.port.out.goal.GoalLoaderPort;
-import com.ddudu.application.port.out.UserLoaderPort;
 import java.util.MissingResourceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class RetrieveGoalService implements RetrieveGoalUseCase {
 
-  private final UserLoaderPort userLoaderPort;
   private final GoalLoaderPort goalLoaderPort;
 
   @Override
