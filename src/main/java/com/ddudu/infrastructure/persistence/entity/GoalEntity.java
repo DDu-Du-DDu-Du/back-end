c1c14b1a424f68a0c39955edcd565eadb0fc66c4
uijin-j <uijin0531@gmail.com>
2024-05-17 19:23:52 +0900

    feat: GoalEntity getter 추가

--- src/main/java/com/ddudu/infrastructure/persistence/entity/GoalEntity.java
+++ src/main/java/com/ddudu/infrastructure/persistence/entity/GoalEntity.java
@@ -18,10 +18,12 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "goals")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GoalEntity extends BaseEntity {
 
