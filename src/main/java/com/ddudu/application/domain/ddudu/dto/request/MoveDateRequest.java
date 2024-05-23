788797bc2fee9d37cb48c1bdca4e3b0f691b50f0
Jay <injaesong0@gmail.com>
2024-05-23 19:56:31 +0900

    feat: 컨트롤러 검증 추가

--- src/main/java/com/ddudu/application/domain/ddudu/dto/request/MoveDateRequest.java
+++ src/main/java/com/ddudu/application/domain/ddudu/dto/request/MoveDateRequest.java
@@ -1,8 +1,10 @@
 package com.ddudu.application.domain.ddudu.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record MoveDateRequest(
+    @NotNull(message = "2011 NULL_DATE_TO_MOVE")
     LocalDate newDate
 ) {
 
--- src/main/java/com/ddudu/presentation/api/controller/DduduController.java
+++ src/main/java/com/ddudu/presentation/api/controller/DduduController.java
@@ -196,6 +196,7 @@ public class DduduController implements DduduControllerDoc {
       @PathVariable
       Long id,
       @RequestBody
+      @Valid
       MoveDateRequest request
   ) {
     return null;
