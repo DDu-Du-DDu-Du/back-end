894755d1f6f75731995148d9123441de7b8cdbea
uijin-j <uijin0531@gmail.com>
2024-05-23 19:02:50 +0900

    feat: DduduLoaderPort에 findAllByDateAndUserAndGoals() 추가

--- src/main/java/com/ddudu/application/port/out/ddudu/DduduLoaderPort.java
+++ src/main/java/com/ddudu/application/port/out/ddudu/DduduLoaderPort.java
@@ -1,9 +1,14 @@
 package com.ddudu.application.port.out.ddudu;
 
 import com.ddudu.application.domain.ddudu.domain.Ddudu;
+import com.ddudu.application.domain.goal.domain.Goal;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface DduduLoaderPort {
 
   Ddudu getDduduOrElseThrow(Long id, String message);
+
+  List<Ddudu> findAllByDateAndUserAndGoals(LocalDate date, Long userId, List<Goal> goals);
 
 }
