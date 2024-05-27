1d6b55bcf2c2e0abb70e0976c666f2e6c9ac5da7
injae <injaesong0@gmail.com>
2024-05-27 15:39:11 +0900

    feat: 뚜두 목록 조회 서비스 구현

---
+++ src/main/java/com/ddudu/application/port/out/ddudu/DduduSearchPort.java
@@ -0,0 +1,15 @@
+package com.ddudu.application.port.out.ddudu;
+
+import com.ddudu.application.dto.ddudu.SimpleDduduSearchDto;
+import com.ddudu.application.dto.scroll.request.ScrollRequest;
+import com.ddudu.application.dto.scroll.response.ScrollResponse;
+
+;
+
+public interface DduduSearchPort {
+
+  ScrollResponse<SimpleDduduSearchDto> search(
+      Long userId, ScrollRequest request, String query, Boolean isMine
+  );
+
+}
