9cb40f403ac10fb6b153a0815d5e5a78c9e5b8a2
injae <injaesong0@gmail.com>
2024-05-27 15:43:12 +0900

    test: 테스트용 픽스쳐 확장

--- src/test/java/com/ddudu/fixture/DduduFixture.java
+++ src/test/java/com/ddudu/fixture/DduduFixture.java
@@ -3,13 +3,30 @@ package com.ddudu.fixture;
 import com.ddudu.application.domain.ddudu.domain.Ddudu;
 import com.ddudu.application.domain.ddudu.domain.enums.DduduStatus;
 import com.ddudu.application.domain.goal.domain.Goal;
+import com.google.common.collect.Lists;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DduduFixture extends BaseFixture {
+
+  public static List<Ddudu> createMultipleDdudusWithGoal(Goal goal, int size) {
+    List<Ddudu> ddudus = Lists.newArrayList();
+
+    for (int i = 0; i < size; i++) {
+      Ddudu ddudu = createDdudu(
+          (long) (i + 1), goal.getId(), goal.getUserId(), getRandomSentenceWithMax(50), null, null,
+          null, null, null, null
+      );
+
+      ddudus.add(ddudu);
+    }
+
+    return ddudus;
+  }
 
   public static Ddudu createRandomDduduWithGoal(Goal goal) {
     return createRandomDduduWithReference(goal.getId(), goal.getUserId(), false, null);
