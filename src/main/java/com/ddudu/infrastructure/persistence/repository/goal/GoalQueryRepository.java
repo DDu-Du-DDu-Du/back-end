7d1a0e0d836d43367ae0cd69f13e25ce6e5bdfa2
injae <injaesong0@gmail.com>
2024-05-31 02:04:26 +0900

    feat: 월별 달성 뚜두 통계 쿼리 구현

--- src/main/java/com/ddudu/infrastructure/persistence/repository/goal/GoalQueryRepository.java
+++ src/main/java/com/ddudu/infrastructure/persistence/repository/goal/GoalQueryRepository.java
@@ -1,8 +1,10 @@
 package com.ddudu.infrastructure.persistence.repository.goal;
 
 import com.ddudu.application.domain.goal.domain.enums.PrivacyType;
+import com.ddudu.application.dto.goal.response.CompletedDduduNumberStatsResponse;
 import com.ddudu.infrastructure.persistence.entity.GoalEntity;
 import com.ddudu.infrastructure.persistence.entity.UserEntity;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface GoalQueryRepository {
@@ -10,5 +12,9 @@ public interface GoalQueryRepository {
   List<GoalEntity> findAllByUser(UserEntity user);
 
   List<GoalEntity> findAllByUserAndPrivacyTypes(UserEntity user, List<PrivacyType> privacyTypes);
+
+  List<CompletedDduduNumberStatsResponse> collectCompletedDduduNumberStats(
+      UserEntity user, LocalDate from, LocalDate to
+  );
 
 }
--- src/main/java/com/ddudu/infrastructure/persistence/repository/goal/GoalQueryRepositoryImpl.java
+++ src/main/java/com/ddudu/infrastructure/persistence/repository/goal/GoalQueryRepositoryImpl.java
@@ -1,13 +1,22 @@
 package com.ddudu.infrastructure.persistence.repository.goal;
 
+import static com.ddudu.infrastructure.persistence.entity.QDduduEntity.dduduEntity;
 import static com.ddudu.infrastructure.persistence.entity.QGoalEntity.goalEntity;
 
+import com.ddudu.application.domain.ddudu.domain.enums.DduduStatus;
 import com.ddudu.application.domain.goal.domain.enums.GoalStatus;
 import com.ddudu.application.domain.goal.domain.enums.PrivacyType;
+import com.ddudu.application.dto.goal.response.CompletedDduduNumberStatsResponse;
 import com.ddudu.infrastructure.persistence.entity.GoalEntity;
 import com.ddudu.infrastructure.persistence.entity.UserEntity;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.core.types.dsl.StringPath;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -42,6 +51,34 @@ public class GoalQueryRepositoryImpl implements GoalQueryRepository {
     return jpaQueryFactory
         .selectFrom(goalEntity)
         .where(whereClause)
+        .fetch();
+  }
+
+  @Override
+  public List<CompletedDduduNumberStatsResponse> collectCompletedDduduNumberStats(
+      UserEntity user, LocalDate from, LocalDate to
+  ) {
+    StringPath completedCountAlias = Expressions.stringPath("completedCount");
+    NumberExpression<Integer> sum = new CaseBuilder().when(
+            dduduEntity.status.eq(DduduStatus.COMPLETE))
+        .then(Expressions.numberTemplate(Integer.class, "1"))
+        .otherwise(Expressions.numberTemplate(Integer.class, "0"))
+        .sum()
+        .as("completedCount");
+
+    return jpaQueryFactory.select(Projections.constructor(
+            CompletedDduduNumberStatsResponse.class,
+            goalEntity.id,
+            goalEntity.name,
+            sum
+        ))
+        .from(goalEntity)
+        .join(dduduEntity)
+        .on(dduduEntity.goal.eq(goalEntity))
+        .where(goalEntity.user.eq(user)
+            .and(dduduEntity.scheduledOn.between(from, to)))
+        .groupBy(goalEntity.id)
+        .orderBy(completedCountAlias.desc())
         .fetch();
   }
 
