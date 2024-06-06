982328ae59765efbe32b29044caa61222f2f7206
uijin-j <uijin0531@gmail.com>
2024-06-07 00:33:40 +0900

    feat: RepeatableDduduDomainService 생성

---
+++ src/main/java/com/ddudu/application/domain/repeatable_ddudu/service/RepeatableDduduDomainService.java
@@ -0,0 +1,55 @@
+package com.ddudu.application.domain.repeatable_ddudu.service;
+
+import com.ddudu.application.annotation.DomainService;
+import com.ddudu.application.domain.ddudu.domain.Ddudu;
+import com.ddudu.application.domain.repeatable_ddudu.domain.RepeatPattern;
+import com.ddudu.application.domain.repeatable_ddudu.domain.RepeatableDdudu;
+import com.ddudu.application.domain.repeatable_ddudu.domain.enums.RepeatType;
+import com.ddudu.application.dto.repeatable_ddudu.requset.CreateRepeatableDduduRequest;
+import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+
+@DomainService
+@RequiredArgsConstructor
+public class RepeatableDduduDomainService {
+
+  public RepeatableDdudu create(CreateRepeatableDduduRequest request) {
+    return RepeatableDdudu.builder()
+        .name(request.name())
+        .goalId(request.goalId())
+        .startDate(request.startDate())
+        .endDate(request.endDate())
+        .repeatType(RepeatType.from(request.repeatType()))
+        .repeatPattern(createRepeatPattern(request))
+        .beginAt(request.beginAt())
+        .endAt(request.endAt())
+        .build();
+  }
+
+  public List<Ddudu> createRepeatedDdudus(Long userId, RepeatableDdudu repeatableDdudu) {
+    return repeatableDdudu.getRepetitionDates()
+        .stream()
+        .map(date -> Ddudu.builder()
+            .name(repeatableDdudu.getName())
+            .goalId(repeatableDdudu.getGoalId())
+            .userId(userId)
+            .repeatableDduduId(repeatableDdudu.getId())
+            .scheduledOn(date)
+            .beginAt(repeatableDdudu.getBeginAt())
+            .endAt(repeatableDdudu.getEndAt())
+            .build()
+        )
+        .toList();
+  }
+
+  private RepeatPattern createRepeatPattern(CreateRepeatableDduduRequest request) {
+    return RepeatPattern.create(
+        Objects.requireNonNull(RepeatType.from(request.repeatType())),
+        request.repeatDays(),
+        request.repeatDates(),
+        request.lastDayOfMonth()
+    );
+  }
+
+}
