f016b6870bdf2424518f3181070b3c70bc01fe02
uijin-j <uijin0531@gmail.com>
2024-06-07 00:08:21 +0900

    feat: WeeklyRepeatPattern 생성

---
+++ src/main/java/com/ddudu/application/domain/repeatable_ddudu/domain/WeeklyRepeatPattern.java
@@ -0,0 +1,29 @@
+package com.ddudu.application.domain.repeatable_ddudu.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class WeeklyRepeatPattern implements RepeatPattern {
+
+  private List<DayOfWeek> repeatedDaysOfWeek;
+
+  @Override
+  public List<LocalDate> calculateRepetitionDates(LocalDate startDate, LocalDate endDate) {
+    return Stream.iterate(startDate, date -> date.plusDays(1))
+        .limit(countDaysBetween(startDate, endDate))
+        .filter(date -> repeatedDaysOfWeek.contains(date.getDayOfWeek()))
+        .toList();
+  }
+
+  private long countDaysBetween(LocalDate startDate, LocalDate endDate) {
+    return startDate.until(endDate)
+        .getDays() + 1;
+  }
+
+}
