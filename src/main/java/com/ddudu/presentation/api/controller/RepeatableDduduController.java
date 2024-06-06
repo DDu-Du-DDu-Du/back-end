748f257983ab63bbb566c23d7fbbf571baa49869
uijin-j <uijin0531@gmail.com>
2024-06-07 00:48:23 +0900

    feat: 반복 뚜두 생성 UseCase와 Controller 연결

--- src/main/java/com/ddudu/presentation/api/controller/RepeatableDduduController.java
+++ src/main/java/com/ddudu/presentation/api/controller/RepeatableDduduController.java
@@ -1,10 +1,12 @@
 package com.ddudu.presentation.api.controller;
 
 import com.ddudu.application.dto.repeatable_ddudu.requset.CreateRepeatableDduduRequest;
+import com.ddudu.application.port.in.repeatable_ddudu.CreateRepeatableDduduUseCase;
 import com.ddudu.presentation.api.annotation.Login;
 import com.ddudu.presentation.api.common.dto.response.IdResponse;
 import com.ddudu.presentation.api.doc.RepeatableDduduControllerDoc;
 import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/repeatitive-ddudus")
+@RequestMapping("/api/repeatable-ddudus")
 @RequiredArgsConstructor
 public class RepeatableDduduController implements RepeatableDduduControllerDoc {
+
+  private final CreateRepeatableDduduUseCase createRepeatableDduduUseCase;
 
   @PostMapping
   public ResponseEntity<IdResponse> create(
@@ -25,8 +29,11 @@ public class RepeatableDduduController implements RepeatableDduduControllerDoc {
       @Valid
       CreateRepeatableDduduRequest request
   ) {
+    Long id = createRepeatableDduduUseCase.create(loginId, request);
+    URI uri = URI.create("/api/repeatable-ddudus/" + id);
 
-    return null;
+    return ResponseEntity.created(uri)
+        .body(new IdResponse(id));
   }
 
 }
