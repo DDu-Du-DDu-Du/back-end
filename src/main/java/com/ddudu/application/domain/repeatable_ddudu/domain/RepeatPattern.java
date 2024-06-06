08cc2c2d40ee46d08140037f7fcb224bb3442fbb
uijin-j <uijin0531@gmail.com>
2024-06-07 00:28:33 +0900

    feat: RepeatPattern에 팩토리 메서드 추가

--- src/main/java/com/ddudu/application/domain/repeatable_ddudu/domain/RepeatPattern.java
+++ src/main/java/com/ddudu/application/domain/repeatable_ddudu/domain/RepeatPattern.java
@@ -1,9 +1,24 @@
 package com.ddudu.application.domain.repeatable_ddudu.domain;
 
+import com.ddudu.application.domain.repeatable_ddudu.domain.enums.RepeatType;
+import com.ddudu.application.domain.repeatable_ddudu.util.DayOfWeekUtil;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface RepeatPattern {
+
+  static RepeatPattern create(
+      RepeatType repeatType,
+      List<String> repeatDays,
+      List<Integer> repeatDates,
+      Boolean lastDayOfMonth
+  ) {
+    return switch (repeatType) {
+      case DAILY -> new DailyRepeatPattern();
+      case WEEKLY -> new WeeklyRepeatPattern(DayOfWeekUtil.toDaysOfWeek(repeatDays));
+      case MONTHLY -> new MonthlyRepeatPattern(repeatDates, lastDayOfMonth);
+    };
+  }
 
   List<LocalDate> calculateRepetitionDates(LocalDate startDate, LocalDate endDate);
 
