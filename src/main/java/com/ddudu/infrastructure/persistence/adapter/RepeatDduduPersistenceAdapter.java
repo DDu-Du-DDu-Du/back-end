6de28491446ef6c8e18dc02370fb7c7a5b413456
uijin-j <uijin0531@gmail.com>
2024-06-09 18:11:58 +0900

    feat: RepeatDduduLoaderPort 생성 및 구현

--- src/main/java/com/ddudu/infrastructure/persistence/adapter/RepeatDduduPersistenceAdapter.java
+++ src/main/java/com/ddudu/infrastructure/persistence/adapter/RepeatDduduPersistenceAdapter.java
@@ -1,15 +1,17 @@
 package com.ddudu.infrastructure.persistence.adapter;
 
 import com.ddudu.application.domain.repeat_ddudu.domain.RepeatDdudu;
+import com.ddudu.application.port.out.repeat_ddudu.RepeatDduduLoaderPort;
 import com.ddudu.application.port.out.repeat_ddudu.SaveRepeatDduduPort;
 import com.ddudu.infrastructure.annotation.DrivenAdapter;
 import com.ddudu.infrastructure.persistence.entity.RepeatDduduEntity;
 import com.ddudu.infrastructure.persistence.repository.repeat_ddudu.RepeatDduduRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @DrivenAdapter
 @RequiredArgsConstructor
-public class RepeatDduduAdapter implements SaveRepeatDduduPort {
+public class RepeatDduduPersistenceAdapter implements SaveRepeatDduduPort, RepeatDduduLoaderPort {
 
   private final RepeatDduduRepository repeatDduduRepository;
 
@@ -17,6 +19,13 @@ public class RepeatDduduAdapter implements SaveRepeatDduduPort {
   public RepeatDdudu save(RepeatDdudu repeatDdudu) {
     return repeatDduduRepository.save(RepeatDduduEntity.from(repeatDdudu))
         .toDomain();
+  }
+
+
+  @Override
+  public Optional<RepeatDdudu> getOptionalRepeatDdudu(Long id) {
+    return repeatDduduRepository.findById(id)
+        .map(RepeatDduduEntity::toDomain);
   }
 
 }
