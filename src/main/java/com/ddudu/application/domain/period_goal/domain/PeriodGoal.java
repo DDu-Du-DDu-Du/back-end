87c07ebfac06e8737dbb31c320e72dc30f40dd35
uijin-j <uijin0531@gmail.com>
2024-06-20 03:01:40 +0900

    feat: PeriodGoal 도메인 클래스 생성

---
+++ src/main/java/com/ddudu/application/domain/period_goal/domain/PeriodGoal.java
@@ -0,0 +1,63 @@
+package com.ddudu.application.domain.period_goal.domain;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.nonNull;
+
+import com.ddudu.application.domain.period_goal.domain.enums.PeriodGoalType;
+import com.ddudu.application.domain.period_goal.domain.vo.PeriodGoalDate;
+import com.ddudu.application.domain.period_goal.exception.PeriodGoalErrorCode;
+import com.ddudu.application.domain.user.domain.User;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public final class PeriodGoal {
+
+  @EqualsAndHashCode.Include
+  private final Long id;
+  private final User user;
+  private final String contents;
+  private final PeriodGoalType type;
+  @Getter(AccessLevel.NONE)
+  private final PeriodGoalDate planDate;
+
+  @Builder
+  public PeriodGoal(
+      Long id, User user, String contents, String type, LocalDate planDate
+  ) {
+    validate(user, contents, type);
+
+    this.id = id;
+    this.user = user;
+    this.contents = contents;
+    this.type = PeriodGoalType.from(type);
+    this.planDate = PeriodGoalDate.of(this.type, planDate);
+  }
+
+  public LocalDate getPlanDate() {
+    return planDate.getDate();
+  }
+
+  private void validate(User user, String contents, String type) {
+    validateUser(user);
+    validateContents(contents);
+    validateType(type);
+  }
+
+  private void validateUser(User user) {
+    checkArgument(nonNull(user), PeriodGoalErrorCode.USER_NOT_EXISTING);
+  }
+
+  private void validateContents(String contents) {
+    checkArgument(nonNull(contents), PeriodGoalErrorCode.CONTENTS_NOT_EXISTING);
+  }
+
+  private void validateType(String type) {
+    checkArgument(nonNull(type), PeriodGoalErrorCode.PERIOD_GOAL_TYPE_NOT_EXISTING);
+  }
+
+}
