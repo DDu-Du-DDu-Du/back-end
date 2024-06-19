df98319696cb22584c35ac50bc3974918aa682fc
uijin-j <uijin0531@gmail.com>
2024-06-20 03:06:06 +0900

    feat: PeriodGoalRepository 생성

---
+++ src/main/java/com/ddudu/infrastructure/persistence/repository/period_goal/PeriodGoalRepository.java
@@ -0,0 +1,8 @@
+package com.ddudu.infrastructure.persistence.repository.period_goal;
+
+import com.ddudu.infrastructure.persistence.entity.PeriodGoalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeriodGoalRepository extends JpaRepository<PeriodGoalEntity, Long> {
+
+}
