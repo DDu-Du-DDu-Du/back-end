c280ec880af8084bd1fae88808990a1a4ce4018d
uijin-j <uijin0531@gmail.com>
2024-06-27 14:41:11 +0900

    feat: 기간 목표 조회 API 문서 작성

--- src/main/java/com/ddudu/presentation/api/doc/PeriodGoalControllerDoc.java
+++ src/main/java/com/ddudu/presentation/api/doc/PeriodGoalControllerDoc.java
@@ -1,12 +1,17 @@
 package com.ddudu.presentation.api.doc;
 
 import com.ddudu.application.dto.period_goal.request.CreatePeriodGoalRequest;
+import com.ddudu.application.dto.period_goal.response.PeriodGoalSummary;
 import com.ddudu.presentation.api.common.dto.response.IdResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDate;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
@@ -22,5 +27,31 @@ public interface PeriodGoalControllerDoc {
       )
   )
   ResponseEntity<IdResponse> create(Long userId, CreatePeriodGoalRequest request);
+
+  @Operation(summary = "기간 목표 조회")
+  @ApiResponse(
+      responseCode = "200",
+      content = @Content(
+          mediaType = MediaType.APPLICATION_JSON_VALUE,
+          schema = @Schema(implementation = PeriodGoalSummary.class)
+      )
+  )
+  @Parameters(
+      {
+          @Parameter(
+              name = "date",
+              description = "기간 목표 날짜 (기본값: 오늘)",
+              in = ParameterIn.QUERY,
+              example = "2024-06-10"
+          ),
+          @Parameter(
+              name = "type",
+              description = "기간 목표 타입 (WEEK | MONTH)",
+              in = ParameterIn.QUERY,
+              example = "WEEK"
+          )
+      }
+  )
+  ResponseEntity<PeriodGoalSummary> getPeriodGoal(Long userId, LocalDate date, String type);
 
 }
