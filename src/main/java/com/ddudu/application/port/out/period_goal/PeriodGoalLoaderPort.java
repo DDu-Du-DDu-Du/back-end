7f282a71c009a1603a1da6018d7a5a8d67b4379e
uijin-j <uijin0531@gmail.com>
2024-06-27 15:47:51 +0900

    feat: 기간 목표 조회 Port 생성

---
+++ src/main/java/com/ddudu/application/port/out/period_goal/PeriodGoalLoaderPort.java
@@ -0,0 +1,13 @@
+package com.ddudu.application.port.out.period_goal;
+
+import com.ddudu.application.domain.period_goal.domain.PeriodGoal;
+import com.ddudu.application.domain.period_goal.domain.enums.PeriodGoalType;
+import com.ddudu.application.domain.user.domain.User;
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface PeriodGoalLoaderPort {
+
+  Optional<PeriodGoal> getOptionalByDate(User user, LocalDate date, PeriodGoalType type);
+
+}
