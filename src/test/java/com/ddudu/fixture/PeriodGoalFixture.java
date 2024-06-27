ffeb59f44fcf5dec2a5661d631ed3e0514f4c112
uijin-j <uijin0531@gmail.com>
2024-06-27 19:09:09 +0900

    test: 기간 목표 수정 테스트 코드 작성

--- src/test/java/com/ddudu/fixture/PeriodGoalFixture.java
+++ src/test/java/com/ddudu/fixture/PeriodGoalFixture.java
@@ -1,6 +1,9 @@
 package com.ddudu.fixture;
 
+import com.ddudu.application.domain.period_goal.domain.PeriodGoal;
 import com.ddudu.application.domain.period_goal.domain.enums.PeriodGoalType;
+import com.ddudu.application.domain.user.domain.User;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +15,16 @@ public class PeriodGoalFixture extends BaseFixture {
     int index = getRandomInt(0, types.length - 1);
 
     return types[index];
+  }
+
+  public static PeriodGoal createRandomPeriodGoal(User user) {
+    return PeriodGoal.builder()
+        .id(getRandomId())
+        .userId(user.getId())
+        .type(getRandomType().name())
+        .contents(getRandomSentenceWithMax(255))
+        .planDate(LocalDate.now())
+        .build();
   }
 
 }
