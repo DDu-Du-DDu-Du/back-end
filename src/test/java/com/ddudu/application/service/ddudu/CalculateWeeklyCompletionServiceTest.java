78234ae799b080cbb15511a33adc7f82785f734b
uijin-j <uijin0531@gmail.com>
2024-07-11 11:11:42 +0900

    test: 불필요한 주석 제거

--- src/test/java/com/ddudu/application/service/ddudu/CalculateWeeklyCompletionServiceTest.java
+++ src/test/java/com/ddudu/application/service/ddudu/CalculateWeeklyCompletionServiceTest.java
@@ -26,9 +26,10 @@ import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-//@Transactional
+@Transactional
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class CalculateWeeklyCompletionServiceTest {
 
