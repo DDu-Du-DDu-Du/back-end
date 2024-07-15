50e3c957a30cb26f8394005195a038a1fe3886d0
uijin-j <uijin0531@gmail.com>
2024-07-15 21:50:11 +0900

    refactor: 불필요한 코드 중복 제거

--- src/main/java/com/ddudu/application/domain/ddudu/domain/Timetable.java
+++ src/main/java/com/ddudu/application/domain/ddudu/domain/Timetable.java
@@ -7,7 +7,6 @@ import com.ddudu.application.domain.goal.domain.Goal;
 import com.ddudu.application.dto.ddudu.BasicDduduWithGoalIdAndTime;
 import com.ddudu.application.dto.ddudu.GoalGroupedDdudus;
 import com.ddudu.application.dto.ddudu.TimeGroupedDdudus;
-import com.ddudu.application.dto.ddudu.response.BasicDduduResponse;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +17,7 @@ import java.util.stream.Collectors;
 public class Timetable {
 
   private final Map<Integer, List<Ddudu>> timetable;
-  private final List<Ddudu> unassignedDdudus;
+  private final DduduList unassignedDdudus;
 
   public Timetable(List<Ddudu> ddudus) {
     assertNotNull(ddudus, "Timetable 생성 시 ddudus는 null일 수 없습니다.");
@@ -29,7 +28,7 @@ public class Timetable {
     List<Ddudu> assignedDdudus = split.getOrDefault(true, new ArrayList<>());
     timetable = assignedDdudus.stream()
         .collect(groupingBy(Ddudu::getBeginHour));
-    unassignedDdudus = split.getOrDefault(false, new ArrayList<>());
+    unassignedDdudus = new DduduList(split.getOrDefault(false, new ArrayList<>()));
   }
 
   private static Map<Long, Goal> mapGoalById(List<Goal> goals) {
@@ -53,24 +52,7 @@ public class Timetable {
   }
 
   public List<GoalGroupedDdudus> getUnassignedDdudusWithGoal(List<Goal> goals) {
-    Map<Long, Goal> goalsById = mapGoalById(goals);
-    Map<Long, List<Ddudu>> ddudusByGoal = mapUnssignedDdudusByGoal();
-
-    return goalsById.entrySet()
-        .stream()
-        .map(entry -> GoalGroupedDdudus.of(
-            entry.getValue(),
-            ddudusByGoal.getOrDefault(entry.getKey(), new ArrayList<>())
-                .stream()
-                .map(BasicDduduResponse::from)
-                .toList()
-        ))
-        .toList();
-  }
-
-  private Map<Long, List<Ddudu>> mapUnssignedDdudusByGoal() {
-    return unassignedDdudus.stream()
-        .collect(groupingBy(Ddudu::getGoalId));
+    return unassignedDdudus.getDdudusWithGoal(goals);
   }
 
 }
