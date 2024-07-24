72b14509c3c8996a5107adcc1367009bbdff8e71
uijin-j <uijin0531@gmail.com>
2024-07-24 19:35:34 +0900

    docs: 불필요한 리스트 형식 제거

--- src/main/java/com/ddudu/presentation/api/doc/AuthControllerDoc.java
+++ src/main/java/com/ddudu/presentation/api/doc/AuthControllerDoc.java
@@ -20,11 +20,7 @@ import org.springframework.http.ResponseEntity;
 public interface AuthControllerDoc {
 
   @Operation(summary = "소셜 로그인")
-  @ApiResponses(
-      value = {
-          @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true)
-      }
-  )
+  @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true)
   @SecurityRequirement(name = "카카오에서 토큰 받아오기")
   @Parameter(
       name = "providerType",
@@ -43,25 +39,20 @@ public interface AuthControllerDoc {
           @ApiResponse(
               responseCode = "401", description = "UNAUTHORIZED",
               content = @Content(
-                  examples = {
-                      @ExampleObject(
-                          name = "5007",
-                          description = "저장된 토큰이 없거나 이미 사용한 토큰인 경우",
-                          value = AuthErrorExamples.AUTH_REFRESH_NOT_ALLOWED
-                      )
-                  }
+                  examples = @ExampleObject(
+                      name = "5007", description = "저장된 토큰이 없거나 이미 사용한 토큰인 경우",
+                      value = AuthErrorExamples.AUTH_REFRESH_NOT_ALLOWED
+                  )
               )
           ),
           @ApiResponse(
               responseCode = "404", description = "NOT_FOUND",
               content = @Content(
-                  examples = {
-                      @ExampleObject(
-                          name = "5008",
-                          description = "토큰의 사용자가 존재하지 않는 경우",
-                          value = AuthErrorExamples.AUTH_USER_NOT_FOUND
-                      )
-                  }
+                  examples = @ExampleObject(
+                      name = "5008",
+                      description = "토큰의 사용자가 존재하지 않는 경우",
+                      value = AuthErrorExamples.AUTH_USER_NOT_FOUND
+                  )
               )
           )
       }
