b361760837e7149000adc58bf34e055ddb122a13
uijin-j <uijin0531@gmail.com>
2024-07-24 20:12:12 +0900

    docs: Repeat Ddudu 관련 400번 에러코드 명세

--- src/main/java/com/ddudu/presentation/api/doc/RepeatDduduControllerDoc.java
+++ src/main/java/com/ddudu/presentation/api/doc/RepeatDduduControllerDoc.java
@@ -22,27 +22,82 @@ public interface RepeatDduduControllerDoc {
               responseCode = "201", description = "CREATED", useReturnTypeSchema = true
           ),
           @ApiResponse(
-              responseCode = "403", description = "FORBIDDEN",
+              responseCode = "400", description = "BAD_REQUEST",
               content = @Content(
                   examples = {
                       @ExampleObject(
-                          name = "6014",
-                          description = "해당 목표에 대한 권한이 없는 경우 (본인만 가능)",
-                          value = GoalErrorExamples.GOAL_INVALID_AUTHORITY
+                          name = "6001",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_BLANK_NAME
+                      ),
+                      @ExampleObject(
+                          name = "6006",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_EXCESSIVE_NAME_LENGTH
+                      ),
+                      @ExampleObject(
+                          name = "6002",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_NULL_GOAL_VALUE
+                      ),
+                      @ExampleObject(
+                          name = "6003",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_NULL_REPEAT_TYPE
+                      ),
+                      @ExampleObject(
+                          name = "6009",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_INVALID_REPEAT_TYPE
+                      ),
+                      @ExampleObject(
+                          name = "6004",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_NULL_START_DATE
+                      ),
+                      @ExampleObject(
+                          name = "6005",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_NULL_END_DATE
+                      ),
+                      @ExampleObject(
+                          name = "6007",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_UNABLE_TO_END_BEFORE_START
+                      ),
+                      @ExampleObject(
+                          name = "6008",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_UNABLE_TO_FINISH_BEFORE_BEGIN
+                      ),
+                      @ExampleObject(
+                          name = "6010",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_INVALID_DAY_OF_WEEK
+                      ),
+                      @ExampleObject(
+                          name = "6011",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_NULL_OR_EMPTY_REPEAT_DATES_OF_MONTH
+                      ),
+                      @ExampleObject(
+                          name = "6012",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_NULL_OR_EMPTY_REPEAT_DAYS_OF_WEEK
+                      ),
+                      @ExampleObject(
+                          name = "6013",
+                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_NULL_LAST_DAY
                       )
                   }
               )
           ),
           @ApiResponse(
+              responseCode = "403", description = "FORBIDDEN",
+              content = @Content(
+                  examples = @ExampleObject(
+                      name = "6014",
+                      description = "해당 목표에 대한 권한이 없는 경우 (본인만 가능)",
+                      value = GoalErrorExamples.GOAL_INVALID_AUTHORITY
+                  )
+              )
+          ),
+          @ApiResponse(
               responseCode = "404", description = "NOT_FOUND",
               content = @Content(
-                  examples = {
-                      @ExampleObject(
-                          name = "6014",
-                          description = "목표 아이디가 유효하지 않은 경우",
-                          value = RepeatDduduErrorExamples.REPEAT_DDUDU_INVALID_GOAL
-                      )
-                  }
+                  examples = @ExampleObject(
+                      name = "6014",
+                      description = "목표 아이디가 유효하지 않은 경우",
+                      value = RepeatDduduErrorExamples.REPEAT_DDUDU_INVALID_GOAL
+                  )
               )
           )
       }
