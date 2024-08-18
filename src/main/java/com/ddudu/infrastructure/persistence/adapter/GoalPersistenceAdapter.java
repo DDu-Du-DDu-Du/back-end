5bfb534bd183023a5ec9889f422edf5b1aebf098
Jay <injaesong0@gmail.com>
2024-08-18 21:29:25 +0900

    refactor: 스탯을 목표가 아닌 뚜두 레퍼지터리 활용

--- src/main/java/com/ddudu/infrastructure/persistence/adapter/GoalPersistenceAdapter.java
+++ src/main/java/com/ddudu/infrastructure/persistence/adapter/GoalPersistenceAdapter.java
@@ -3,10 +3,8 @@ package com.ddudu.infrastructure.persistence.adapter;
 import com.ddudu.application.domain.goal.domain.Goal;
 import com.ddudu.application.domain.goal.domain.enums.PrivacyType;
 import com.ddudu.application.domain.user.domain.User;
-import com.ddudu.application.dto.goal.response.CompletedDduduNumberStatsResponse;
 import com.ddudu.application.port.out.goal.DeleteGoalPort;
 import com.ddudu.application.port.out.goal.GoalLoaderPort;
-import com.ddudu.application.port.out.goal.NumberStatsPort;
 import com.ddudu.application.port.out.goal.SaveGoalPort;
 import com.ddudu.application.port.out.goal.UpdateGoalPort;
 import com.ddudu.infrastructure.annotation.DrivenAdapter;
@@ -16,8 +14,6 @@ import com.ddudu.infrastructure.persistence.repository.ddudu.DduduRepository;
 import com.ddudu.infrastructure.persistence.repository.goal.GoalRepository;
 import com.google.common.collect.Lists;
 import jakarta.persistence.EntityNotFoundException;
-import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 import java.util.MissingResourceException;
 import java.util.Optional;
@@ -26,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @DrivenAdapter
 @RequiredArgsConstructor
 public class GoalPersistenceAdapter implements SaveGoalPort, GoalLoaderPort, UpdateGoalPort,
-    DeleteGoalPort, NumberStatsPort {
+    DeleteGoalPort {
 
   private final GoalRepository goalRepository;
   private final DduduRepository dduduRepository;
@@ -72,14 +68,6 @@ public class GoalPersistenceAdapter implements SaveGoalPort, GoalLoaderPort, Upd
         .stream()
         .map(GoalEntity::toDomain)
         .toList();
-  }
-
-  @Override
-  public List<CompletedDduduNumberStatsResponse> collectNumberStats(
-      User user, LocalDate from, LocalDate to
-  ) {
-    return Collections.unmodifiableList(
-        goalRepository.collectCompletedDduduNumberStats(UserEntity.from(user), from, to));
   }
 
   @Override
--- src/main/java/com/ddudu/infrastructure/persistence/repository/goal/GoalQueryRepository.java
+++ src/main/java/com/ddudu/infrastructure/persistence/repository/goal/GoalQueryRepository.java
@@ -1,10 +1,8 @@
 package com.ddudu.infrastructure.persistence.repository.goal;
 
 import com.ddudu.application.domain.goal.domain.enums.PrivacyType;
-import com.ddudu.application.dto.goal.response.CompletedDduduNumberStatsResponse;
 import com.ddudu.infrastructure.persistence.entity.GoalEntity;
 import com.ddudu.infrastructure.persistence.entity.UserEntity;
-import java.time.LocalDate;
 import java.util.List;
 
 public interface GoalQueryRepository {
@@ -12,9 +10,5 @@ public interface GoalQueryRepository {
   List<GoalEntity> findAllByUser(UserEntity user);
 
   List<GoalEntity> findAllByUserAndPrivacyTypes(UserEntity user, List<PrivacyType> privacyTypes);
-
-  List<CompletedDduduNumberStatsResponse> collectCompletedDduduNumberStats(
-      UserEntity user, LocalDate from, LocalDate to
-  );
 
 }
