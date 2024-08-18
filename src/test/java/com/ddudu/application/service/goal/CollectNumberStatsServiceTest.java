c4cc3796a5c4529ebedbafe9404721f0229374ff
Jay <injaesong0@gmail.com>
2024-08-18 21:31:27 +0900

    feat: 종합 통계 비즈니스 로직 구현

--- src/test/java/com/ddudu/application/service/goal/CollectNumberStatsServiceTest.java
+++ src/test/java/com/ddudu/application/service/goal/CollectNumberStatsServiceTest.java
@@ -2,30 +2,39 @@ package com.ddudu.application.service.goal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.doReturn;
 
 import com.ddudu.application.domain.ddudu.domain.Ddudu;
 import com.ddudu.application.domain.ddudu.domain.enums.DduduStatus;
 import com.ddudu.application.domain.goal.domain.Goal;
 import com.ddudu.application.domain.goal.exception.GoalErrorCode;
 import com.ddudu.application.domain.user.domain.User;
-import com.ddudu.application.dto.goal.response.CompletedDduduNumberStatsResponse;
+import com.ddudu.application.dto.goal.response.MonthlyStatsSummaryResponse;
 import com.ddudu.application.port.out.auth.SignUpPort;
 import com.ddudu.application.port.out.ddudu.SaveDduduPort;
 import com.ddudu.application.port.out.goal.SaveGoalPort;
+import com.ddudu.application.service.ddudu.CollectMonthlyStatsSummaryService;
 import com.ddudu.fixture.DduduFixture;
 import com.ddudu.fixture.GoalFixture;
 import com.ddudu.fixture.UserFixture;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.MissingResourceException;
+import java.util.Optional;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.auditing.AuditingHandler;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -33,8 +42,14 @@ import org.springframework.transaction.annotation.Transactional;
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class CollectNumberStatsServiceTest {
 
+  @MockBean
+  DateTimeProvider dateTimeProvider;
+
+  @SpyBean
+  AuditingHandler auditingHandler;
+
   @Autowired
-  CollectNumberStatsService collectNumberStatsService;
+  CollectMonthlyStatsSummaryService collectMonthlyStatsService;
 
   @Autowired
   SignUpPort signUpPort;
@@ -52,10 +67,16 @@ class CollectNumberStatsServiceTest {
 
   @BeforeEach
   void setUp() {
+    MockitoAnnotations.openMocks(this);
+    auditingHandler.setDateTimeProvider(dateTimeProvider);
+    doReturn(Optional.of(LocalDateTime.now())).when(dateTimeProvider)
+        .getNow();
+
     user = signUpPort.save(UserFixture.createRandomUserWithId());
     goals = saveGoalPort.saveAll(GoalFixture.createRandomGoalsWithUser(user, 5));
     uncompletedCountPerGoal = 5;
     completedCountPerGoal = 5;
+
     goals.forEach(goal -> saveDduduPort.saveAll(
         DduduFixture.createDifferentDdudusWithGoal(goal, completedCountPerGoal,
             uncompletedCountPerGoal
@@ -63,57 +84,61 @@ class CollectNumberStatsServiceTest {
   }
 
   @Test
-  void 이번달_달성_뚜두_수의_통계를_낸다() {
+  void 이번달_뚜두_통합_통계를_낸다() {
     // given
     YearMonth thisMonth = YearMonth.now();
+    LocalDateTime lastMonth = LocalDateTime.now()
+        .minusMonths(1);
+
+    doReturn(Optional.of(lastMonth)).when(dateTimeProvider)
+        .getNow();
+    goals.forEach(goal -> saveDduduPort.saveAll(
+        DduduFixture.createDifferentDdudusWithGoal(goal, completedCountPerGoal,
+            uncompletedCountPerGoal
+        )));
 
     // when
-    List<CompletedDduduNumberStatsResponse> response = collectNumberStatsService.collectNumberStats(
+    MonthlyStatsSummaryResponse response = collectMonthlyStatsService.collectMonthlyTotalStats(
         user.getId(), thisMonth);
 
     // then
-    assertThat(response).hasSize(goals.size());
-    assertThat(response).allMatch(stats -> stats.completedCount()
-        .equals(completedCountPerGoal));
+    int totalPerGoal = uncompletedCountPerGoal + completedCountPerGoal;
+    System.out.println(response);
+
+    assertThat(response.lastMonth())
+        .hasFieldOrPropertyWithValue("yearMonth", YearMonth.from(lastMonth))
+        .hasFieldOrPropertyWithValue("totalCount", totalPerGoal * goals.size());
+    assertThat(response.thisMonth())
+        .hasFieldOrPropertyWithValue("yearMonth", thisMonth)
+        .hasFieldOrPropertyWithValue("totalCount", totalPerGoal * goals.size());
   }
 
   @Test
-  void 요청의_날짜가_없으면_이번달의_달성_뚜두_수의_통계를_낸다() {
+  void 요청의_날짜가_없으면_이번달의_뚜두_통계를_낸다() {
     // given
+    LocalDateTime lastMonth = LocalDateTime.now()
+        .minusMonths(1);
+
+    doReturn(Optional.of(lastMonth)).when(dateTimeProvider)
+        .getNow();
+    goals.forEach(goal -> saveDduduPort.saveAll(
+        DduduFixture.createDifferentDdudusWithGoal(goal, completedCountPerGoal,
+            uncompletedCountPerGoal
+        )));
 
     // when
-    List<CompletedDduduNumberStatsResponse> response = collectNumberStatsService.collectNumberStats(
+    MonthlyStatsSummaryResponse response = collectMonthlyStatsService.collectMonthlyTotalStats(
         user.getId(), null);
 
     // then
-    assertThat(response).hasSize(goals.size());
-    assertThat(response).allMatch(stats -> stats.completedCount()
-        .equals(completedCountPerGoal));
-  }
+    int totalPerGoal = uncompletedCountPerGoal + completedCountPerGoal;
 
-  @Test
-  void 이번달_달성_뚜두_수의_통계는_내림차순이다() {
-    // given
-    YearMonth thisMonth = YearMonth.now();
-    int extraForThird = 3;
-    int extraForSecond = 2;
-    Goal thirdGoal = goals.get(2);
-    Goal secondGoal = goals.get(1);
-
-    saveDduduPort.saveAll(DduduFixture.createMultipleDdudusWithGoal(thirdGoal, extraForThird));
-    saveDduduPort.saveAll(DduduFixture.createMultipleDdudusWithGoal(secondGoal, extraForSecond));
-
-    // when
-    List<CompletedDduduNumberStatsResponse> response = collectNumberStatsService.collectNumberStats(
-        user.getId(), thisMonth);
-
-    // then
-    assertThat(response.get(0))
-        .hasFieldOrPropertyWithValue("id", thirdGoal.getId())
-        .hasFieldOrPropertyWithValue("completedCount", completedCountPerGoal + extraForThird);
-    assertThat(response.get(1))
-        .hasFieldOrPropertyWithValue("id", secondGoal.getId())
-        .hasFieldOrPropertyWithValue("completedCount", completedCountPerGoal + extraForSecond);
+    assertThat(response.lastMonth())
+        .hasFieldOrPropertyWithValue("yearMonth", YearMonth.from(lastMonth))
+        .hasFieldOrPropertyWithValue("totalCount", totalPerGoal * goals.size());
+    assertThat(response.thisMonth())
+        .hasFieldOrPropertyWithValue("yearMonth", YearMonth.now())
+        .hasFieldOrPropertyWithValue("totalCount", totalPerGoal * goals.size());
   }
 
   @Test
@@ -121,35 +146,35 @@ class CollectNumberStatsServiceTest {
     // given
     YearMonth nextMonth = YearMonth.now()
         .plusMonths(1);
+    Ddudu ddudu = DduduFixture.createRandomDduduWithStatusAndSchedule(
+        goals.get(0), DduduStatus.COMPLETE, nextMonth.atDay(1));
+
+    doReturn(Optional.of(LocalDate.now())).when(dateTimeProvider)
+        .getNow();
+    saveDduduPort.save(ddudu);
 
     // when
-    List<CompletedDduduNumberStatsResponse> response = collectNumberStatsService.collectNumberStats(
+    MonthlyStatsSummaryResponse response = collectMonthlyStatsService.collectMonthlyTotalStats(
         user.getId(), nextMonth);
 
     // then
-    assertThat(response).isEmpty();
+    assertThat(response.thisMonth()
+        .totalCount()).isZero();
   }
 
   @Test
-  void 통계_대상_월에_뚜두를_생성했지만_달성한_뚜두가_없으면_0개의_통계를_낸다() {
+  void 지난달_뚜두가_없어도_통계는_0으로_계산된다() {
     // given
-    YearMonth nextMonth = YearMonth.now()
-        .plusMonths(1);
-    Goal firstGoal = goals.get(0);
-    Ddudu extra = saveDduduPort.save(
-        DduduFixture.createRandomDduduWithStatusAndSchedule(firstGoal, DduduStatus.UNCOMPLETED,
-            LocalDate.now()
-                .plusMonths(1)
-        ));
+    YearMonth lastMonth = YearMonth.now()
+        .minusMonths(1);
 
     // when
-    List<CompletedDduduNumberStatsResponse> response = collectNumberStatsService.collectNumberStats(
-        user.getId(), nextMonth);
+    MonthlyStatsSummaryResponse response = collectMonthlyStatsService.collectMonthlyTotalStats(
+        user.getId(), lastMonth);
 
     // then
-    assertThat(response).hasSize(1);
-    assertThat(response).allMatch(stats -> stats.completedCount()
-        .equals(0));
+    assertThat(response.lastMonth()
+        .totalCount()).isZero();
   }
 
   @Test
@@ -159,7 +184,7 @@ class CollectNumberStatsServiceTest {
     YearMonth thisMonth = YearMonth.now();
 
     // when
-    ThrowingCallable collect = () -> collectNumberStatsService.collectNumberStats(
+    ThrowingCallable collect = () -> collectMonthlyStatsService.collectMonthlyTotalStats(
         invalidId, thisMonth);
 
     // then
