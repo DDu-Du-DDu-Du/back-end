b621d713e2a636ed46ca3347c7d7b17eac6bc6bf
Jay <injaesong0@gmail.com>
2024-08-24 11:59:23 +0900

    docs: 월별 통합 뚜두 통계 문서 디테일 추가

--- src/main/java/com/ddudu/presentation/api/doc/StatsControllerDoc.java
+++ src/main/java/com/ddudu/presentation/api/doc/StatsControllerDoc.java
@@ -1,8 +1,13 @@
 package com.ddudu.presentation.api.doc;
 
 import com.ddudu.application.dto.goal.response.MonthlyStatsSummaryResponse;
+import com.ddudu.presentation.api.doc.error.AuthErrorExamples;
+import com.ddudu.presentation.api.doc.error.DduduErrorExamples;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.YearMonth;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +18,36 @@ import org.springframework.http.ResponseEntity;
 )
 public interface StatsControllerDoc {
 
-  @Operation(summary = "월별 달성 뚜두 수 통계")
-  @ApiResponse(
-      responseCode = "200"
+  @Operation(summary = "월별 통합 뚜두 통계")
+  @ApiResponses(
+      {
+          @ApiResponse(
+              responseCode = "200",
+              description = "OK",
+              useReturnTypeSchema = true
+          ),
+          @ApiResponse(
+              responseCode = "401",
+              description = "UNAUTHORIZED",
+              content = @Content(
+                  examples = @ExampleObject(
+                      name = "5002",
+                      value = AuthErrorExamples.AUTH_BAD_TOKEN_CONTENT
+                  )
+              )
+          ),
+          @ApiResponse(
+              responseCode = "404",
+              description = "NOT_FOUND",
+              content = @Content(
+                  examples = @ExampleObject(
+                      name = "2006",
+                      description = "로그인 사용자 아이디가 유효하지 않은 경우",
+                      value = DduduErrorExamples.DDUDU_USER_NOT_EXISTING
+                  )
+              )
+          )
+      }
   )
   ResponseEntity<MonthlyStatsSummaryResponse> collectSummary(
       Long loginId, YearMonth yearMonth
