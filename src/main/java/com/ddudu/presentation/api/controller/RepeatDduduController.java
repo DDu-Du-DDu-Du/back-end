cf72276b954b38579cf28a822afd5bc8412d316a
uijin-j <uijin0531@gmail.com>
2024-09-12 15:43:12 +0900

    feat: 반복뚜두 삭제 API 엔트포인트 추가

--- src/main/java/com/ddudu/presentation/api/controller/RepeatDduduController.java
+++ src/main/java/com/ddudu/presentation/api/controller/RepeatDduduController.java
@@ -3,6 +3,7 @@ package com.ddudu.presentation.api.controller;
 import com.ddudu.application.dto.repeat_ddudu.request.CreateRepeatDduduRequest;
 import com.ddudu.application.dto.repeat_ddudu.request.UpdateRepeatDduduRequest;
 import com.ddudu.application.port.in.repeat_ddudu.CreateRepeatDduduUseCase;
+import com.ddudu.application.port.in.repeat_ddudu.DeleteRepeatDduduUseCase;
 import com.ddudu.application.port.in.repeat_ddudu.UpdateRepeatDduduUseCase;
 import com.ddudu.presentation.api.annotation.Login;
 import com.ddudu.presentation.api.common.dto.response.IdResponse;
@@ -11,6 +12,7 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,6 +27,7 @@ public class RepeatDduduController implements RepeatDduduControllerDoc {
 
   private final CreateRepeatDduduUseCase createRepeatDduduUseCase;
   private final UpdateRepeatDduduUseCase updateRepeatDduduUseCase;
+  private final DeleteRepeatDduduUseCase deleteRepeatDduduUseCase;
 
   @PostMapping
   public ResponseEntity<IdResponse> create(
@@ -54,6 +57,19 @@ public class RepeatDduduController implements RepeatDduduControllerDoc {
     Long repeatDduduId = updateRepeatDduduUseCase.update(loginId, id, request);
 
     return ResponseEntity.ok(new IdResponse(repeatDduduId));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(
+      @Login
+      Long loginId,
+      @PathVariable
+      Long id
+  ) {
+    deleteRepeatDduduUseCase.delete(loginId, id);
+    
+    return ResponseEntity.noContent()
+        .build();
   }
 
 }
