6069dca1933fa0010efcb14537baeff063d278a5
injaesong <injaesong0@gmail.com>
2025-05-08 22:46:03 +0900

    chore: JUnit Platform 강제 적용

--- buildSrc/src/main/kotlin/ddudu.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/ddudu.java-conventions.gradle.kts
@@ -29,7 +29,8 @@ dependencies {
 
     // test
     testImplementation("org.assertj:assertj-core:3.27.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.13.0-M3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
