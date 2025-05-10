0e87c30ae5b2ff3a33d2dc7f8ccdd86d26d8f63c
injaesong <injaesong0@gmail.com>
2025-05-10 15:47:46 +0900

    test: user application 모듈 테스트 시 app context 생성 용 spring boot application 주체 생성

---
+++ application/user-application/src/test/java/com/ddudu/UserApplicationTestConfig.java
@@ -0,0 +1,9 @@
+package com.ddudu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class UserApplicationTestConfig {
+
+}
