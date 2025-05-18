7ee8ae2f86d1f40a3c9adacdda040028647bb6e1
injaesong <injaesong0@gmail.com>
2025-05-18 20:05:15 +0900

    refactor: 통계 에러코드 예시 수정

---
+++ bootstrap/bootstrap-common/src/main/java/com/ddudu/bootstrap/common/doc/examples/StatsErrorExamples.java
@@ -0,0 +1,26 @@
+package com.ddudu.bootstrap.common.doc.examples;
+
+public final class StatsErrorExamples {
+
+  public static final String STATS_INVALID_DDUDU_STATS = """
+      {
+        "code": 9001,
+        "message": "유효한 뚜두 상태가 아닙니다."
+      }
+      """;
+
+  public static final String STATS_LOGIN_USER_NOT_FOUND = """
+      {
+        "code": 9002,
+        "message": "로그인 사용자가 존재하지 않습니다."
+      }
+      """;
+
+  public static final String STATS_USER_NOT_FOUND = """
+      {
+        "code": 9003,
+        "message": "사용자가 존재하지 않습니다."
+      }
+      """;
+
+}
