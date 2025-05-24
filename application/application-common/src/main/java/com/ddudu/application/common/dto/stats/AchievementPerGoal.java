92d98837459beffc57ca97f4f1bce119f09b1820
injaesong <injaesong0@gmail.com>
2025-05-24 18:32:27 +0900

    feat: 월 목표 별 달성률 통계 유즈케이스 구현

---
+++ application/application-common/src/main/java/com/ddudu/application/common/dto/stats/AchievementPerGoal.java
@@ -0,0 +1,17 @@
+package com.ddudu.application.common.dto.stats;
+
+import com.ddudu.aggregate.MonthlyStats;
+import lombok.Builder;
+
+@Builder
+public record AchievementPerGoal(Long goalId, String goalName, Integer achievementPercentage) {
+
+  public static AchievementPerGoal from(MonthlyStats monthlyStats) {
+    return AchievementPerGoal.builder()
+        .goalId(monthlyStats.getGoalId())
+        .goalName(monthlyStats.getGoalName())
+        .achievementPercentage(monthlyStats.calculateAchievementPercentage())
+        .build();
+  }
+
+}
