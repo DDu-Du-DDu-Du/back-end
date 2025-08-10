de1c47bd5e2a701a1d430a316441548115f5a636
injaesong <injaesong0@gmail.com>
2025-08-10 12:13:19 +0900

    feat: 월별 상세통계 서비스 구현

---
+++ application/application-common/src/main/java/com/ddudu/application/common/dto/stats/response/AchievedStatsDetailResponse.java
@@ -0,0 +1,19 @@
+package com.ddudu.application.common.dto.stats.response;
+
+import com.ddudu.application.common.dto.stats.AchievedDetailOverviewDto;
+import com.ddudu.application.common.dto.stats.DayOfWeekStatsDto;
+import com.ddudu.application.common.dto.stats.GenericCalendarStats;
+import com.ddudu.application.common.dto.stats.RepeatDduduStatsDto;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record AchievedStatsDetailResponse(
+    Long goalId,
+    AchievedDetailOverviewDto overview,
+    DayOfWeekStatsDto dayOfWeekStats,
+    List<RepeatDduduStatsDto> repeatDduduStats,
+    GenericCalendarStats<DduduCompletionResponse> completions
+) {
+
+}
--- common/src/main/java/com/ddudu/common/exception/StatsErrorCode.java
+++ common/src/main/java/com/ddudu/common/exception/StatsErrorCode.java
@@ -10,7 +10,10 @@ public enum StatsErrorCode implements ErrorCode {
   LOGIN_USER_NOT_EXISTING(9002, "로그인 사용자가 존재하지 않습니다."),
   USER_NOT_EXISTING(9003, "사용자가 존재하지 않습니다."),
   MONTHLY_STATS_EMPTY(9004, "월 통계 데이터가 없습니다."),
-  MONTHLY_STATS_NOT_GROUPED_BY_GOAL(9005, "다른 목표의 스탯이 포함되어 있습니다.")
+  MONTHLY_STATS_NOT_GROUPED_BY_GOAL(9005, "다른 목표의 스탯이 포함되어 있습니다."),
+  INVALID_TO_MONTH(9006, "끝나는 월은 시작 월보다 빠를 수 없습니다."),
+  NULL_GOAL_ID(9007, "상세통계 조회 대상 목표 아이디는 필수값입니다."),
+  GOAL_NOT_EXISTING(9008, "상세통계 조회 대상 목표를 찾을 수 없습니다.")
   ;
 
   private final int code;
