754a2fee7ed3450b7c26d81376b9d4a29b286ea1
injaesong <injaesong0@gmail.com>
2025-09-01 18:21:07 +0900

    feat: 뚜두 예외코드 추가

--- common/src/main/java/com/ddudu/common/exception/DduduErrorCode.java
+++ common/src/main/java/com/ddudu/common/exception/DduduErrorCode.java
@@ -22,7 +22,11 @@ public enum DduduErrorCode implements ErrorCode {
   UNABLE_TO_REPRODUCE_ON_SAME_DATE(2013, "다시 하기의 날짜는 기존과 달라야합니다."),
   NEGATIVE_OR_ZERO_GOAL_ID(2014, "목표 ID는 양수입니다."),
   NULL_SCHEDULED_DATE(2015, "날짜는 필수값입니다."),
-  GOAL_ALREADY_DONE(2016, "목표가 이미 완료되었습니다.");
+  GOAL_ALREADY_DONE(2016, "목표가 이미 완료되었습니다."),
+  BEGIN_AT_REQUIRED_FOR_REMINDER(2017, "시작 시간 없이 미리알림을 설정할 수 없습니다."),
+  REMINDER_NOT_AFTER_NOW(2018, "미리알림 시간은 현재 시간보다 이후여야 합니다."),
+  ZERO_REMINDER(2019, "미리알림 입력값이 없습니다."),
+  NEGATIVE_REMINDER_INPUT_EXISTS(2020, "미리알림을 입력값 중 음수가 포함되어 있습니다.");
 
   private final int code;
   private final String message;
