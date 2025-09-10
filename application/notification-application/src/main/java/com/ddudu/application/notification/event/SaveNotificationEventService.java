e5e52c692c1c531b4345efd6dba86a7a95f895c0
injaesong <injaesong0@gmail.com>
2025-09-10 23:48:11 +0900

    feat: 알림 이벤트 저장 서비스 구현

---
+++ application/notification-application/src/main/java/com/ddudu/application/notification/event/SaveNotificationEventService.java
@@ -0,0 +1,55 @@
+package com.ddudu.application.notification.event;
+
+import com.ddudu.application.common.dto.notification.event.NotificationEventSaveEvent;
+import com.ddudu.application.common.port.notification.in.SaveNotificationEventUseCase;
+import com.ddudu.application.common.port.notification.out.NotificationEventCommandPort;
+import com.ddudu.application.common.port.notification.out.NotificationEventLoaderPort;
+import com.ddudu.common.annotation.UseCase;
+import com.ddudu.domain.notification.event.aggregate.NotificationEvent;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@UseCase
+@RequiredArgsConstructor
+@Transactional
+public class SaveNotificationEventService implements SaveNotificationEventUseCase {
+
+  private final NotificationEventCommandPort notificationEventCommandPort;
+  private final NotificationEventLoaderPort notificationEventLoaderPort;
+
+  @Override
+  public void save(NotificationEventSaveEvent event) {
+    NotificationEvent notificationEvent = upsertEvent(event);
+
+    if (notificationEvent.isPlannedToday()) {
+      // TODO: Task Scheduler 구현 후 예약 구현
+    }
+  }
+
+  private NotificationEvent upsertEvent(NotificationEventSaveEvent event) {
+    Optional<NotificationEvent> optionalEvent = notificationEventLoaderPort.getOptionalEventByContext(
+        event.userId(),
+        event.typeCode(),
+        event.contextId()
+    );
+
+    if (optionalEvent.isEmpty()) {
+      NotificationEvent notificationEvent = NotificationEvent.builder()
+          .contextId(event.contextId())
+          .typeCode(event.typeCode())
+          .receiverId(event.userId())
+          .senderId(event.userId())
+          .willFireAt(event.willFireAt())
+          .build();
+
+      return notificationEventCommandPort.save(notificationEvent);
+    }
+
+    NotificationEvent notificationEvent = optionalEvent.get()
+        .updateFireTime(event.willFireAt());
+
+    return notificationEventCommandPort.update(notificationEvent);
+  }
+
+}
