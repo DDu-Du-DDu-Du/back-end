19872e00e7f7f3ae44372515e71c48aedb9bd8d4
injaesong <injaesong0@gmail.com>
2025-09-12 19:12:25 +0900

    feat: 알림 이벤트 당일 스케쥴링 발생 시 스케쥴링 포트 호출

--- application/notification-application/src/main/java/com/ddudu/application/notification/event/RemoveNotificationEventService.java
+++ application/notification-application/src/main/java/com/ddudu/application/notification/event/RemoveNotificationEventService.java
@@ -4,6 +4,7 @@ import com.ddudu.application.common.dto.notification.event.NotificationEventRemo
 import com.ddudu.application.common.port.notification.in.RemoveNotificationEventUseCase;
 import com.ddudu.application.common.port.notification.out.NotificationEventCommandPort;
 import com.ddudu.application.common.port.notification.out.NotificationEventLoaderPort;
+import com.ddudu.application.common.port.notification.out.NotificationSchedulingPort;
 import com.ddudu.common.annotation.UseCase;
 import com.ddudu.domain.notification.event.aggregate.NotificationEvent;
 import java.util.Optional;
@@ -17,6 +18,7 @@ public class RemoveNotificationEventService implements RemoveNotificationEventUs
 
   private final NotificationEventLoaderPort notificationEventLoaderPort;
   private final NotificationEventCommandPort notificationEventCommandPort;
+  private final NotificationSchedulingPort notificationSchedulingPort;
 
   @Override
   public void remove(NotificationEventRemoveEvent event) {
@@ -39,7 +41,7 @@ public class RemoveNotificationEventService implements RemoveNotificationEventUs
     notificationEventCommandPort.delete(notificationEvent);
 
     if (notificationEvent.isPlannedToday()) {
-      // TODO: Task Scheduler 구현 후 예약 취소 구현
+      notificationSchedulingPort.cancelNotificationEvent(notificationEvent.getId());
     }
   }
 
