f48f94ed9d58350c85ea51a97d98bffede2a04bd
injaesong <injaesong0@gmail.com>
2026-03-10 01:37:33 +0900

    chore(notification): localize announcement error messages to Korean

--- bootstrap/bootstrap-common/src/main/java/com/ddudu/bootstrap/common/doc/examples/AnnouncementErrorExamples.java
+++ bootstrap/bootstrap-common/src/main/java/com/ddudu/bootstrap/common/doc/examples/AnnouncementErrorExamples.java
@@ -9,49 +9,49 @@ public final class AnnouncementErrorExamples {
   public static final String NULL_TITLE = """
       {
         "code": 13001,
-        "message": "Announcement title is required."
+        "message": "공지사항 제목은 필수값입니다."
       }
       """;
 
   public static final String EXCESSIVE_TITLE_LENGTH = """
       {
         "code": 13002,
-        "message": "Announcement title must be 50 characters or fewer."
+        "message": "공지사항 제목은 최대 50자입니다."
       }
       """;
 
   public static final String NULL_CONTENTS = """
       {
         "code": 13003,
-        "message": "Announcement contents are required."
+        "message": "공지사항 내용은 필수값입니다."
       }
       """;
 
   public static final String EXCESSIVE_CONTENTS_LENGTH = """
       {
         "code": 13004,
-        "message": "Announcement contents must be 2000 characters or fewer."
+        "message": "공지사항 내용은 최대 2000자입니다."
       }
       """;
 
   public static final String LOGIN_USER_NOT_EXISTING = """
       {
         "code": 13006,
-        "message": "Login user does not exist."
+        "message": "로그인 사용자가 존재하지 않습니다."
       }
       """;
 
   public static final String INVALID_AUTHORITY = """
       {
         "code": 13007,
-        "message": "User is not authorized to perform this action."
+        "message": "해당 기능에 대한 사용자 권한이 없습니다."
       }
       """;
 
   public static final String ANNOUNCEMENT_NOT_EXISTING = """
       {
         "code": 13008,
-        "message": "Announcement does not exist."
+        "message": "해당 아이디의 공지사항이 존재하지 않습니다."
       }
       """;
 
