35e750c731f896ba0ca337753fab03fcaf06cd1c
injaesong <injaesong0@gmail.com>
2026-03-10 00:08:27 +0900

    feat(notification-api): add announcement scroll list endpoint

--- bootstrap/notification-api/src/main/java/com/ddudu/api/notification/announcement/controller/AnnouncementController.java
+++ bootstrap/notification-api/src/main/java/com/ddudu/api/notification/announcement/controller/AnnouncementController.java
@@ -2,12 +2,17 @@ package com.ddudu.api.notification.announcement.controller;
 
 import com.ddudu.api.notification.announcement.doc.AnnouncementControllerDoc;
 import com.ddudu.application.common.dto.IdResponse;
+import com.ddudu.application.common.dto.notification.SimpleAnnouncementDto;
+import com.ddudu.application.common.dto.notification.request.AnnouncementSearchRequest;
 import com.ddudu.application.common.dto.notification.request.CreateAnnouncementRequest;
+import com.ddudu.application.common.dto.scroll.response.ScrollResponse;
+import com.ddudu.application.common.port.notification.in.AnnouncementSearchUseCase;
 import com.ddudu.application.common.port.notification.in.CreateAnnouncementUseCase;
 import com.ddudu.bootstrap.common.annotation.Login;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnnouncementController implements AnnouncementControllerDoc {
 
   private final CreateAnnouncementUseCase createAnnouncementUseCase;
+  private final AnnouncementSearchUseCase announcementSearchUseCase;
+
+  @Override
+  @GetMapping
+  public ResponseEntity<ScrollResponse<SimpleAnnouncementDto>> getList(
+      AnnouncementSearchRequest request
+  ) {
+    ScrollResponse<SimpleAnnouncementDto> response = announcementSearchUseCase.search(request);
+
+    return ResponseEntity.ok(response);
+  }
 
   @Override
   @PostMapping
