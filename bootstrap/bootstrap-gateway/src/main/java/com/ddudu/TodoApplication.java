fcaf504bc439a06392763f53d496a1d92493591e
Injae Song <110002292+IjjS@users.noreply.github.com>
2026-03-31 20:42:40 +0900

    서버 기본 타임존 UTC 설정 적용

--- bootstrap/bootstrap-gateway/src/main/java/com/ddudu/TodoApplication.java
+++ bootstrap/bootstrap-gateway/src/main/java/com/ddudu/TodoApplication.java
@@ -1,5 +1,6 @@
 package com.ddudu;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     SpringApplication.run(TodoApplication.class, args);
   }
 
