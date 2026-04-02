c18ffe80dba6fc273876ec9dd5cc4d621155a5f8
Injae Song <110002292+IjjS@users.noreply.github.com>
2026-04-02 12:28:11 +0900

    test: 제거된 리마인더 로직 테스트 정리 및 DTO 시그니처 반영

--- application/planning-application/src/test/java/com/ddudu/application/planning/ddudu/service/UpdateTodoServiceTest.java
+++ application/planning-application/src/test/java/com/ddudu/application/planning/ddudu/service/UpdateTodoServiceTest.java
@@ -61,8 +61,6 @@ class UpdateTodoServiceTest {
         LocalDate.now().plusDays(1),
         LocalTime.of(10, 0),
         LocalTime.of(11, 0),
-        null,
-        null,
         null
     );
   }
